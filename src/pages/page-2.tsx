d26d31d9939096f760c93ba5f7f0b8fa67409344
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-07-17 13:05:02 +0200

    improvement: add jest & @testing-library

--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import { Link } from 'gatsby'
+import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const SecondPage = () => (
+const SecondPage = (): JSX.Element => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
