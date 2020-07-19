838dd59cf3f677843291c275f87c476b1bbd0b1d
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-07-19 22:49:39 +0200

    improvement: run gatsby-** as TypeScript module

---
+++ .gatsby/gatsby-node.ts
@@ -0,0 +1,74 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+import { SourceNodesArgs } from 'gatsby'
+import { ITSConfigFn } from 'gatsby-plugin-ts-config'
+import { GetPostsSlugQuery } from '../typings/graphql-types'
+
+const gatsbyNode: ITSConfigFn<'node'> = () => ({
+  createPages: async ({ actions, graphql, reporter }) => {
+    const result = await graphql<GetPostsSlugQuery>(`
+      query GetPostsSlug {
+        allMdx {
+          nodes {
+            frontmatter {
+              slug
+            }
+          }
+        }
+      }
+    `)
+
+    if (result.errors) {
+      reporter.panic('fail to create posts', result.errors)
+    }
+
+    const posts = result.data?.allMdx?.nodes
+
+    posts.forEach((post) => {
+      actions.createPage({
+        path: post.frontmatter.slug,
+        component: require.resolve('../src/templates/post.tsx'),
+        context: {
+          slug: post.frontmatter.slug,
+        },
+      })
+    })
+  },
+})
+
+
+export default gatsbyNode
+
+// const createPages = async ({ actions, graphql, reporter }) => {
+//   const result = await graphql(`
+//     query GetPostsSlug {
+//       allMdx {
+//         nodes {
+//           frontmatter {
+//             slug
+//           }
+//         }
+//       }
+//     }
+//   `)
+
+//   if (result.errors) {
+//     reporter.panic('fail to create posts', result.errors)
+//   }
+
+//   const posts = result.data?.allMdx?.nodes
+
+//   posts.forEach((post) => {
+//     actions.createPage({
+//       path: post.frontmatter.slug,
+//       component: require.resolve('./src/templates/post.tsx'),
+//       context: {
+//         slug: post.frontmatter.slug,
+//       },
+//     })
+//   })
+// }
