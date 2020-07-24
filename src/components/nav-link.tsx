614a19f2ef912b4d09d6aa6fb4ab9638dba34477
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-07-24 18:26:07 +0200

    refactor(nav-desktop): compartamentize desktop navigation

--- src/components/nav-link.tsx
+++ src/components/nav-link.tsx
@@ -1,36 +1,19 @@
 import styled from '@emotion/styled'
-// import { Link } from 'gatsby'
+import { Link } from 'gatsby'
 
-import { bpMaxSM } from '../lib/breakpoints'
+export const NavLink = styled(Link)`
+  padding: 0.25rem;
 
-import { HeaderLink } from './header-link'
+  color: inherit;
+  text-decoration: none;
 
-// export const NavLink = styled(Link)<{ fontWeight?: string }>`
-//   margin: 0 0.5rem 0 0;
-//   padding: 0.25rem;
+  &:hover,
+  &:active {
+    text-decoration: none;
+  }
 
-//   color: #222;
-//   font-weight: ${({ fontweight }) => fontweight || 'normal'};
-//   font-size: 1rem;
-//   line-height: 1;
-//   text-decoration: none;
+  &.current-page {
+    border-bottom: 2px solid #222;
+  }
 
-//   &.current-page {
-//     border-bottom: 2px solid #222;
-//   }
-
-//   &:last-of-type {
-//     margin-right: 0;
-//   }
-// `
-
-export const NavLink = styled(HeaderLink)({
-  background: 'transparent',
-  borderRadius: '3px',
-  marginTop: 'unset',
-  padding: '8px 10px',
-  // media query
-  /* stylelint-disable-line */ [bpMaxSM]: {
-    display: 'none',
-  },
-})
+`
