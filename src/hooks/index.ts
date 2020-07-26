522e626d414baeda47c91288f448f4e3135a4435
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-07-26 23:34:55 +0200

    feat(resume): [WIP] - parse and dispaly the resume.json

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { usePostsPreview } from './use-posts'
 export { useSiteMetadata } from './use-sitemetadata'
 export { useProfilePictureFix } from './use-profile-picture-fix'
+export { useResume } from './use-resume'
