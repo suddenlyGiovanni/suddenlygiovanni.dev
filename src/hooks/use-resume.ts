bd0263176796425c4c95edd251a2fd46a3db243c
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-07-28 22:26:51 +0200

    improvement(resume.json): add personal data to resume object

--- src/hooks/use-resume.ts
+++ src/hooks/use-resume.ts
@@ -4,7 +4,7 @@
 import { graphql, useStaticQuery } from 'gatsby'
 
 import { ResumeQuery } from '../../typings/graphql-types'
-import { Resume } from '../../typings/resume'
+import { Education, Resume, Work } from '../../typings/resume'
 
 const resumeQuery = graphql`
   query Resume {
@@ -41,15 +41,6 @@ const resumeQuery = graphql`
         summary
         url
       }
-      volunteer {
-        endDate
-        highlights
-        organization
-        position
-        startDate
-        summary
-        url
-      }
       education {
         area
         courses
@@ -58,32 +49,6 @@ const resumeQuery = graphql`
         institution
         startDate
         studyType
-        url
-      }
-      projects {
-        description
-        endDate
-        endDate
-        entity
-        highlights
-        keywords
-        name
-        roles
-        startDate
-        type
-        url
-      }
-      awards {
-        awarder
-        date
-        summary
-        title
-      }
-      publications {
-        name
-        publisher
-        releaseDate
-        summary
         url
       }
       skills {
@@ -99,10 +64,6 @@ const resumeQuery = graphql`
         keywords
         name
       }
-      references {
-        name
-        reference
-      }
       meta {
         canonical
         lastModified
@@ -112,52 +73,61 @@ const resumeQuery = graphql`
   }
 `
 
+type QueryEducation = import('/Users/webdev/Projects/personal/suddenlygiovanni/typings/graphql-types').Maybe<
+  Pick<
+    import('/Users/webdev/Projects/personal/suddenlygiovanni/typings/graphql-types').ResumeJsonEducation,
+    | 'url'
+    | 'area'
+    | 'courses'
+    | 'endDate'
+    | 'gpa'
+    | 'institution'
+    | 'startDate'
+    | 'studyType'
+  >
+>
+const mapEducation = (ed: QueryEducation): Education =>
+  ({
+    ...ed,
+    endDate: new Date(ed?.endDate),
+    startDate: new Date(ed?.startDate),
+  } as Education) // TODO: remove type casting
+
+type QueryWork = import('/Users/webdev/Projects/personal/suddenlygiovanni/typings/graphql-types').Maybe<
+  Pick<
+    import('/Users/webdev/Projects/personal/suddenlygiovanni/typings/graphql-types').ResumeJsonWork,
+    | 'name'
+    | 'summary'
+    | 'url'
+    | 'endDate'
+    | 'startDate'
+    | 'description'
+    | 'highlights'
+    | 'location'
+    | 'position'
+  >
+>
+
+const mapWork = (w: QueryWork): Work => {
+  return {
+    ...w,
+    endDate: new Date(w?.endDate),
+    startDate: new Date(w?.startDate),
+  } as Work // TODO: remove type casting
+}
 export const useResume = (): Resume => {
   const { resumeJson } = useStaticQuery<ResumeQuery>(resumeQuery)
   const resume: Resume = {
-    awards: resumeJson?.awards?.map((award) => ({
-      ...award,
-      date: new Date(award?.date),
-    })),
     basics: resumeJson?.basics,
-    education: resumeJson?.education?.map((ed) => ({
-      ...ed,
-      endDate: new Date(ed?.endDate),
-      startDate: new Date(ed?.startDate),
-    })),
+    education: resumeJson?.education?.map(mapEducation),
     interests: resumeJson?.interests,
     languages: resumeJson?.languages,
     meta: resumeJson?.meta && {
       ...resumeJson.meta,
       lastModified: new Date(resumeJson.meta.lastModified),
     },
-    projects:
-      resumeJson?.projects?.map((pr) => ({
-        ...pr,
-        endDate: new Date(pr?.endDate),
-        startDate: new Date(pr?.startDate),
-      })) || [],
-    publications:
-      resumeJson?.publications?.map((pub) => ({
-        ...pub,
-        releaseDate: new Date(pub?.releaseDate),
-      })) || [],
-    references: resumeJson?.references || [],
     skills: resumeJson?.skills || [],
-    volunteer:
-      resumeJson?.volunteer?.map((vol) => ({
-        ...vol,
-        endDate: new Date(vol?.endDate),
-        startDate: new Date(vol?.startDate),
-      })) || [],
-    work:
-      resumeJson?.work?.map((w) => {
-        return {
-          ...w,
-          endDate: new Date(w?.endDate),
-          startDate: new Date(w?.startDate),
-        }
-      }) || [],
+    work: resumeJson?.work?.map(mapWork) || [],
   } as Resume // TODO: remove this temporary casting!
   return resume
 }
