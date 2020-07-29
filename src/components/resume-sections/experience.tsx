53f47239eaa88f2479a8344fa8d2f5132286f7a7
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-07-29 19:19:35 +0200

    improvement(resume/experience): adopt Template for experience section of resume

--- src/components/resume-sections/experience.tsx
+++ src/components/resume-sections/experience.tsx
@@ -1,21 +1,14 @@
-import { css } from '@emotion/core'
 import React from 'react'
-import { FaExternalLinkAlt } from 'react-icons/fa'
 
 import type { Work } from '../../../typings/resume'
+
+import { Template } from './template'
 
 type Props = {
   works: Work[]
 }
 
 export function Experience({ works }: Props): JSX.Element {
-  function formatDate(date: Date): string {
-    return date.toLocaleDateString('en-US', {
-      month: 'short',
-      year: 'numeric',
-    })
-  }
-
   return (
     <section>
       <h2>Experience</h2>
@@ -31,126 +24,20 @@ export function Experience({ works }: Props): JSX.Element {
           summary,
           url,
         }) => (
-          <dl
+          <Template
             key={name}
-            css={css`
-              border: solid thin;
-            `}
-          >
-            <dt
-              css={css`
-                display: flex;
-                flex-direction: column;
-                width: 100%;
-                span {
-                  display: flex;
-                  flex-direction: row;
-                  align-items: center;
-
-                  color: var(--text-muted);
-
-                  font-weight: normal;
-                  font-size: 0.9rem;
-                  font-style: italic;
-                }
-              `}
-            >
-              {/* POSITION eg CEO/President */}
-              <h3
-                aria-label="job title"
-                css={css`
-                  margin-top: unset;
-                  margin-bottom: unset;
-
-                  font-weight: bold;
-                  font-size: 1rem;
-                  line-height: unset;
-                `}
-              >
-                {position}
-              </h3>
-              {/* NAME eg Facebook */}
-              <span
-                aria-label="company"
-                css={css`
-                  color: var(--body-color) !important;
-                  font-size: 1rem !important;
-                  font-style: unset !important;
-                `}
-              >
-                {name}
-                <a
-                  href={url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  css={css`
-                    margin-left: 0.5rem;
-                  `}
-                >
-                  <FaExternalLinkAlt aria-label="link to external resource" />
-                </a>
-              </span>
-              <span
-                css={css`
-                  justify-content: space-between;
-                `}
-              >
-                {/* START DATE / END DATE */}
-                <span aria-label="start date / end date">
-                  <time
-                    dateTime={startDate?.toISOString()}
-                    css={css`
-                      margin-right: 0.5rem;
-                    `}
-                  >
-                    {startDate && formatDate(startDate)}
-                  </time>
-                  {endDate && (
-                    <>
-                      -
-                      <time
-                        dateTime={endDate.toISOString()}
-                        css={css`
-                          margin-left: 0.5rem;
-                        `}
-                      >
-                        {formatDate(endDate)}
-                      </time>
-                    </>
-                  )}
-                </span>
-                {/* LOCATION */}
-                <span aria-label="company location">{location}</span>
-              </span>
-
-              {/* DESCRIPTION */}
-              {description && (
-                <span aria-label="company description">{description}</span>
-              )}
-            </dt>
-            {/* SUMMARY */}
-            <dd>{summary}</dd>
-
-            {/* HIGHLIGHTS */}
-            <dd>
-              <ul
-                css={css`
-                  margin-bottom: unset;
-                  margin-left: unset;
-
-                  list-style-type: none;
-                  li {
-                    /* margin-bottom: unset; */
-                    padding-left: unset;
-                  }
-                `}
-              >
-                {highlights?.map((highlight, i) => (
-                  <li key={`${i}${highlight[0]}`}>{highlight}</li>
-                ))}
-              </ul>
-            </dd>
-          </dl>
+            heading1={position || ''}
+            heading1AriaLabel="job title"
+            heading2={name || ''}
+            heading2AriaLabel="company"
+            heading2Link={url}
+            startDate={startDate}
+            endDate={endDate}
+            location={location}
+            description={description}
+            summary={summary || ''}
+            highlights={highlights || []}
+          />
         )
       )}
     </section>
