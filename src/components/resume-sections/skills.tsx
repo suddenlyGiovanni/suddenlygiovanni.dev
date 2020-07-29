6cb3129766ff1ec51cb93685442705c30e6e7fbd
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-07-29 16:56:08 +0200

    improvement(skills): add template to handle skills section (first pass, TODO: fix the styling)

--- src/components/resume-sections/skills.tsx
+++ src/components/resume-sections/skills.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/core'
 import React from 'react'
 
 import type { Skill } from '../../../typings/resume'
@@ -9,9 +10,33 @@ type Props = {
 export function Skills({ skills }: Props): JSX.Element {
   return (
     <section>
-      <h6>SKILLS</h6>
+      <h2>SKILLS</h2>
       {skills.map((skill) => (
-        <pre>{JSON.stringify(skill, null, 2)}</pre>
+        <dl key={skill.name}>
+          <dt>{skill.name}</dt>
+          <dd>
+            <ul
+              css={css`
+                display: flex;
+                flex-flow: row wrap;
+                align-items: flex-start;
+                justify-content: flex-start;
+                margin-bottom: unset;
+                margin-left: unset;
+
+                list-style: none;
+                li {
+                  margin-right: 1rem;
+                  margin-bottom: unset;
+                }
+              `}
+            >
+              {skill.keywords?.map((keyword) => (
+                <li key={keyword}>{keyword}</li>
+              ))}
+            </ul>
+          </dd>
+        </dl>
       ))}
     </section>
   )
