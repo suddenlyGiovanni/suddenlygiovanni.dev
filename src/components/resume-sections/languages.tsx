9815ae4314eab31c1016d335857145eb83dc9173
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-07-29 21:45:14 +0200

    improvement(language): add language template to resume

--- src/components/resume-sections/languages.tsx
+++ src/components/resume-sections/languages.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/core'
 import React from 'react'
 
 import type { Language } from '../../../typings/resume'
@@ -9,10 +10,29 @@ type Props = {
 export function Languages({ languages }: Props): JSX.Element {
   return (
     <section>
-      <h6>LANGUAGES</h6>
-      {languages.map((language) => (
-        <pre>{JSON.stringify(languages, null, 2)}</pre>
-      ))}
+      <h2>Languages</h2>
+      <dl>
+        {languages.map(({ fluency, language }) => (
+          <div
+            key={language}
+            css={css`
+              display: flex;
+              align-items: center;
+              justify-content: flex-start;
+              dt,
+              dd {
+                margin-bottom: unset;
+              }
+              dd {
+                margin-left: 1rem;
+              }
+            `}
+          >
+            <dt>{language}</dt>
+            <dd>{fluency}</dd>
+          </div>
+        ))}
+      </dl>
     </section>
   )
 }
