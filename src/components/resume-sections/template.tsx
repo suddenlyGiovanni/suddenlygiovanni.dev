8bc05f34996ada19c3f7010cfbd25420a62b187c
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-07-29 18:39:58 +0200

    improvement: [WIP] - add Template for education and work section

---
+++ src/components/resume-sections/template.tsx
@@ -0,0 +1,155 @@
+import { css } from '@emotion/core'
+import React from 'react'
+import { FaExternalLinkAlt } from 'react-icons/fa'
+
+import { formatDate } from '../../lib/helpers'
+
+type Props = {
+  heading1: string
+  heading2: string
+  heading2Link?: string
+  startDate?: Date
+  endDate?: Date
+  location?: string
+  description?: string
+  summary: string
+  highlights: string[]
+}
+
+export function Template({
+  heading1,
+  heading2,
+  heading2Link,
+  startDate,
+  endDate,
+  location,
+  description,
+  summary,
+  highlights,
+}: Props): JSX.Element {
+  return (
+    <dl
+      css={css`
+        border: solid thin;
+      `}
+    >
+      <dt
+        css={css`
+          display: flex;
+          flex-direction: column;
+          width: 100%;
+          span {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+
+            color: var(--text-muted);
+
+            font-weight: normal;
+            font-size: 0.9rem;
+            font-style: italic;
+          }
+        `}
+      >
+        {/* HEADING 1 */}
+        <h3
+          aria-label="job title"
+          css={css`
+            margin-top: unset;
+            margin-bottom: unset;
+
+            font-weight: bold;
+            font-size: 1rem;
+            line-height: unset;
+          `}
+        >
+          {heading1}
+        </h3>
+
+        {/* HEADING 2 */}
+        <span
+          aria-label="company"
+          css={css`
+            color: var(--body-color) !important;
+            font-size: 1rem !important;
+            font-style: unset !important;
+          `}
+        >
+          {heading2}
+          <a
+            href={heading2Link}
+            target="_blank"
+            rel="noopener noreferrer"
+            css={css`
+              margin-left: 0.5rem;
+            `}
+          >
+            <FaExternalLinkAlt aria-label="link to external resource" />
+          </a>
+        </span>
+        <span
+          css={css`
+            justify-content: space-between;
+          `}
+        >
+          {/* START DATE / END DATE */}
+          {startDate && (
+            <span aria-label="start date / end date">
+              <time
+                dateTime={startDate?.toISOString()}
+                css={css`
+                  margin-right: 0.5rem;
+                `}
+              >
+                {formatDate(startDate)}
+              </time>
+              {endDate && (
+                <>
+                  -
+                  <time
+                    dateTime={endDate.toISOString()}
+                    css={css`
+                      margin-left: 0.5rem;
+                    `}
+                  >
+                    {formatDate(endDate)}
+                  </time>
+                </>
+              )}
+            </span>
+          )}
+
+          {/* LOCATION */}
+          {location && <span aria-label="company location">{location}</span>}
+        </span>
+
+        {/* DESCRIPTION */}
+        {description && (
+          <span aria-label="company description">{description}</span>
+        )}
+      </dt>
+      {/* SUMMARY */}
+      <dd>{summary}</dd>
+
+      {/* HIGHLIGHTS */}
+      <dd>
+        <ul
+          css={css`
+            margin-bottom: unset;
+            margin-left: unset;
+
+            list-style-type: none;
+            li {
+              /* margin-bottom: unset; */
+              padding-left: unset;
+            }
+          `}
+        >
+          {highlights?.map((highlight, i) => (
+            <li key={`${i}${highlight[0]}`}>{highlight}</li>
+          ))}
+        </ul>
+      </dd>
+    </dl>
+  )
+}
