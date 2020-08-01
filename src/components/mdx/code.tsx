cec3dda9d7e2ad70d303d8aeacacdb00614a528c
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-08-01 22:46:14 +0200

    improvement(code): add copy code to clipboard

--- src/components/mdx/code.tsx
+++ src/components/mdx/code.tsx
@@ -6,7 +6,11 @@ import type { Language } from 'prism-react-renderer'
 import theme from 'prism-react-renderer/themes/nightOwl'
 import React from 'react'
 
+import { copyToClipboard } from '../../utils/copy-to-clipboard'
+
 const Pre = styled.pre`
+  position: relative;
+
   margin: 1em 0;
   padding: 0.5em;
   overflow-x: auto;
@@ -39,11 +43,37 @@ const LineContent = styled.span`
   display: table-cell;
 `
 
+const CopyCode = styled.button`
+  position: absolute;
+  top: 0.25rem;
+  right: 0.25rem;
+  z-index: auto;
+
+  margin: 0.25em;
+
+  border: 0;
+  border-radius: 3px;
+  opacity: 0.3;
+  &:hover {
+    opacity: 1;
+  }
+`
 type Props = {
   codeString: string
   language: Language
 }
 export const Code = ({ codeString, language }: Props): JSX.Element => {
+  const handleClick = (
+    _event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): void => {
+    copyToClipboard(codeString).catch((error: unknown) => {
+      // eslint-disable-next-line no-console
+      console.error(
+        "I'm sorry, but something wrong happened while trying to copy this code example.\nTry with an other browser",
+        error
+      )
+    })
+  }
   return (
     <Highlight
       Prism={Prism}
@@ -53,6 +83,7 @@ export const Code = ({ codeString, language }: Props): JSX.Element => {
     >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <Pre className={className} style={style}>
+          <CopyCode onClick={handleClick}>Copy</CopyCode>
           {tokens.map((line, i) => (
             <Line key={i} {...getLineProps({ line, key: i })}>
               <LineNo>{i + 1}</LineNo>
