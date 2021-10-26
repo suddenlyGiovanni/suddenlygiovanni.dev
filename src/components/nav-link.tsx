d7be4ef5642c54e275e8e69fdc58498774a728b5
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2021-10-26 21:50:48 +0200

    feat(components): add styled nav-link

---
+++ src/components/nav-link.tsx
@@ -0,0 +1,38 @@
+import { Link } from 'gatsby'
+import styled, { StyledComponent } from 'styled-components'
+
+const NavLink: StyledComponent<
+  typeof Link,
+  any,
+  { $disabled: boolean },
+  '$disabled'
+> = styled(Link).attrs(({ $disabled }: { $disabled?: boolean }) => ({
+  $disabled: $disabled ?? false,
+}))`
+  padding: 0.25rem;
+
+  color: inherit;
+  text-decoration: ${({ $disabled }) =>
+    $disabled ? ('line-through' as const) : ('none' as const)};
+
+  cursor: ${({ $disabled }) =>
+    $disabled ? ('default' as const) : ('not-allowed' as const)};
+
+  &:hover {
+    text-decoration: ${({ $disabled }) =>
+      $disabled ? ('line-through' as const) : ('none' as const)};
+
+    cursor: ${({ $disabled }) => ($disabled ? 'default' : 'not-allowed')};
+  }
+
+  &:hover,
+  &:active {
+    text-decoration: none;
+  }
+
+  &.current-page {
+    border-bottom: 2px solid #222;
+  }
+`
+
+export default NavLink
