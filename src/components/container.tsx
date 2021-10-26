ccd042cc25cb1c851ff715886f356052846d61c4
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2021-10-26 22:03:10 +0200

    feat(components): add general purpose responsive container

---
+++ src/components/container.tsx
@@ -0,0 +1,49 @@
+import styled, { StyledComponent } from 'styled-components'
+import Breakpoints from '../lib/breakpoints'
+
+interface ContainerProps {
+  readonly $maxWidth: number
+  readonly $noHorizontalPadding: boolean
+  readonly $noVerticalPadding: boolean
+}
+
+export const Container: StyledComponent<
+  'div',
+  any,
+  Partial<ContainerProps>,
+  '$maxWidth' | '$noHorizontalPadding' | '$noVerticalPadding'
+> = styled.div.attrs((props: ContainerProps) => ({
+  $maxWidth: props.$maxWidth ?? 720,
+  $noHorizontalPadding: props.$noHorizontalPadding ?? false,
+  $noVerticalPadding: props.$noVerticalPadding ?? false,
+}))`
+  width: 100%;
+
+  max-width: ${({ $maxWidth, $noHorizontalPadding }) => {
+    // @ts-ignore
+    return $maxWidth + ($noHorizontalPadding ? 0 : 80)
+  }}px;
+
+  margin: 0 auto;
+
+  padding-top: ${({ $noVerticalPadding }) => ($noVerticalPadding ? 0 : 40)}px;
+  padding-right: ${({ $noHorizontalPadding }) =>
+    $noHorizontalPadding ? 0 : 40}px;
+  padding-bottom: ${({ $noVerticalPadding }) =>
+    $noVerticalPadding ? 0 : 40}px;
+  padding-left: ${({ $noHorizontalPadding }) =>
+    $noHorizontalPadding ? 0 : 40}px;
+
+  ${Breakpoints.mediaQuerySmallDevices} {
+    padding-top: ${({ $noVerticalPadding }) => ($noVerticalPadding ? 0 : 20)}px;
+
+    padding-right: ${({ $noHorizontalPadding }) =>
+      $noHorizontalPadding ? 0 : 20}px;
+
+    padding-bottom: ${({ $noVerticalPadding }) =>
+      $noVerticalPadding ? 0 : 20}px;
+
+    padding-left: ${({ $noHorizontalPadding }) =>
+      $noHorizontalPadding ? 0 : 20}px;
+  }
+`
