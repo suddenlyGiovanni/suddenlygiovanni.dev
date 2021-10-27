be7f2ca88dd06d407bc80767a84c9eaff68024fe
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2021-10-27 19:53:31 +0200

    feat(layouts): add Layout

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,3 +1,4 @@
 export * from './main'
 export * from './footer'
 export * from './header'
+export * from './layout'
---
+++ src/layouts/layout.tsx
@@ -0,0 +1,41 @@
+// import { MDXProvider } from '@mdx-js/react'
+import * as React from 'react'
+import styled, { StyledComponent } from 'styled-components'
+
+// import { mdxComponents } from '../mdx'
+import { SEO } from '../components'
+
+import GlobalStyles from '../lib/global-styles'
+import { Footer } from './footer'
+import { Header } from './header'
+import { Main } from './main'
+
+/**
+ * FIXME: find a more descriptive name
+ */
+const DivStyled: StyledComponent<'div', any, {}, never> = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  width: 100%;
+  min-width: 320px;
+  min-height: 100vh;
+`
+
+interface Props {
+  customSEO?: boolean
+}
+
+export const Layout: React.FC<Props> = ({ children, customSEO = false }) => (
+  <>
+    <GlobalStyles />
+    {!customSEO && <SEO />}
+    <DivStyled>
+      <Header />
+      {/*<MDXProvider components={mdxComponents}>*/}
+      <Main>{children}</Main>
+      {/*</MDXProvider>*/}
+      <Footer />
+    </DivStyled>
+  </>
+)
