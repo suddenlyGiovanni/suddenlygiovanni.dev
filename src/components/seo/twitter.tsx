9a0992d170ef3b31ab6a709823123284b3bde0fd
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2021-10-27 19:44:39 +0200

    feat(components): add SEO

---
+++ src/components/seo/twitter.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import { Helmet } from 'react-helmet'
+
+interface Props {
+  username: string
+  title: string
+  desc: string
+  image: string
+  type?: string
+}
+
+export const Twitter: React.VFC<Props> = ({
+  type = 'summary_large_image',
+  username,
+  title,
+  desc,
+  image,
+}) => (
+  <Helmet>
+    {username && <meta name="twitter:creator" content={username} />}
+    <meta name="twitter:card" content={type} />
+    <meta name="twitter:title" content={title} />
+    <meta name="twitter:description" content={desc} />
+    <meta name="twitter:image" content={image} />
+    <meta name="twitter:image:alt" content={desc} />
+  </Helmet>
+)
