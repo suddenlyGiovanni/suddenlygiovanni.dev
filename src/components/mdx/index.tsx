05ee57ab77545507cb93f972acdc6a6367a53a92
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2021-10-28 20:21:09 +0200

    refactor(components/mdx): add mdx code support

--- src/components/mdx/index.tsx
+++ src/components/mdx/index.tsx
@@ -1,10 +1,11 @@
 import type { MDXProviderComponents } from '@mdx-js/react'
-// import { Code } from './code'
-// import { Pre } from './pre'
-// import { preToCodeBlock } from './pre-to-code-block'
+import * as React from 'react'
+import { Code } from './code'
 import { List } from './list'
 import { ListItem } from './list-item'
 import { Paragraph } from './paragraph'
+import { Pre } from './pre'
+import { preToCodeBlock } from './pre-to-code-block'
 import { SmallTitle } from './small-title'
 import { Subtitle } from './subtitle'
 import { Title } from './title'
@@ -18,13 +19,15 @@ export const mdxComponents: MDXProviderComponents = {
   p: Paragraph,
   ul: List,
   li: ListItem,
-  // pre: (preProps): JSX.Element => {
-  //   const props = preToCodeBlock(preProps)
-  //   // if there's a codeString and some props, we passed the test
-  //   if (props) {
-  //     return <Code {...props} />
-  //   }
-  //   // it's possible to have a pre without a code in it
-  //   return <Pre {...preProps} />
-  // },
+  pre: (
+    preProps: React.ComponentProps<React.FunctionComponent<any>>
+  ): JSX.Element => {
+    const props = preToCodeBlock(preProps)
+    // if there's a codeString and some props, we passed the test
+    if (props) {
+      return <Code {...props} />
+    }
+    // it's possible to have a pre without a code in it
+    return <Pre {...preProps} />
+  },
 }
