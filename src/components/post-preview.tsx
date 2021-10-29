ed1d81328c3db6d220e68a8650ac3ef35128d078
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2021-10-29 21:15:34 +0200

    feat(components): add PostPreview

---
+++ src/components/post-preview.tsx
@@ -0,0 +1,56 @@
+import { Link } from 'gatsby'
+import * as React from 'react'
+import styled from 'styled-components'
+
+import * as Integers from '../lib/integer'
+
+import { DateAndReadingTime } from './date-and-reading-time'
+
+const PostPreviewHeadingStyled = styled.h2`
+  margin-top: unset;
+`
+
+const LinkStyled = styled(Link)`
+  color: unset;
+  text-decoration: none;
+
+  box-shadow: none;
+
+  &:visited {
+    color: unset;
+  }
+`
+
+interface Props {
+  id: string
+  slug: string
+  author: string
+  title: string
+  description: string
+  date: Date
+  timeToRead: number
+}
+
+export const PostPreview: React.VFC<Props> = ({
+  slug,
+  title,
+  description,
+  date,
+  ...props
+}) => {
+  const timeToRead = Integers.fromNumber(props.timeToRead)
+  return (
+    <article>
+      <header>
+        <PostPreviewHeadingStyled>
+          <LinkStyled to={slug}>{title}</LinkStyled>
+        </PostPreviewHeadingStyled>
+        <DateAndReadingTime date={date} timeToRead={timeToRead} />
+      </header>
+
+      <section>
+        <p>{description}</p>
+      </section>
+    </article>
+  )
+}
