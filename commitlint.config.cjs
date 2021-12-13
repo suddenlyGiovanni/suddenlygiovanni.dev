dcb0864d257eae9c56b7f56c052eb225b440da02
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2021-12-13 22:04:37 +0100

    chore(workspace): cut the first changeset

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,14 +1,20 @@
-/**
- * @type {import('@commitlint/types/lib/load').UserConfig}
- */
-module.exports = {
-  extends: [
-    '@commitlint/config-conventional'
-  ],
-  rules: {
-    'scope-enum': [2, 'always', ['workspace', 'open-graph-protocol', 'open-graph-protocol-react', 'utils']],
-    'scope-case': [1, 'always', 'kebab-case'],
-    'scope-empty': [1, 'never']
-  }
+// @ts-check
 
+/**
+ * @type { import('@commitlint/types/lib/load.d.ts').UserConfig }
+ */
+const Config = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [
+      2,
+      'always',
+      ['workspace', 'open-graph-protocol', 'open-graph-protocol-react', 'utils'],
+    ],
+    'scope-case': [1, 'always', 'kebab-case'],
+    'scope-empty': [1, 'never'],
+  },
 }
+
+
+module.exports = Config
