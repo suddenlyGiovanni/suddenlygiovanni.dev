e5c37903977e0e12843fb3a77e8c43cd8b6fdf7e
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2021-12-19 20:11:49 +0100

    docs(open-graph-protocol-utils): document `ValueOf` in types.ts module

--- packages/open-graph-protocol-utils/src/types.ts
+++ packages/open-graph-protocol-utils/src/types.ts
@@ -1,7 +1,24 @@
 /**
+ * A generic utility type that operates on ObjectType and will return codomain of all its values as a disjoint union.
+ * Optionally it accepts the union type some assignable keys to narrow down the codomain.
+ *
+ * @example
+ * ```ts
+ * type Dictionary = Record<string, number>
+ * ValueOf<Dictionary>
+ * // ^^^ number
+ * ```
+ *
+ * ```ts
+ * interface ObjectType {
+ *   foo: 'FOO';
+ *   ultimateQuestionOfLifeTheUniverseAndEverything: 42;
+ *   greet: <T extends string>(name: T) => `Hi ${T}!`
+ * }
+ * ValueOf<ObjectType, 'ultimateQuestionOfLifeTheUniverseAndEverything' | 'greet'>
+ * // ^^^ 42 | <T extends string>(name: T) => `Hi ${T}!`
+ * ```
+ *
  * @public
  */
-export type ValueOf<
-  ObjectType extends object,
-  ValueType extends keyof ObjectType = keyof ObjectType
-> = ObjectType[ValueType]
+export type ValueOf<T extends object, K extends keyof T = keyof T> = T[K]
