4ece176cece58138023bdf3e5990dbd36dfbf284
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-02-08 20:23:26 +0100

    Update Storybook version to 8.0.0-beta.1
    The commit includes updates to various Storybook dependencies in the pnpm-lock.yaml file. The specified version for the Storybook addons and the main Storybook package have been changed from 7.6.13 to 8.0.0-beta.1. This update also includes the necessary removals and additions to the lockfile sections associated with these dependencies.

--- apps/docs/.storybook/main.ts
+++ apps/docs/.storybook/main.ts
@@ -1,14 +1,15 @@
+import { dirname, join } from 'node:path'
 import type { StorybookConfig } from '@storybook/react-vite'
 
 const config = {
 	stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(ts|tsx)'],
 	addons: [
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@storybook/addon-interactions',
+		getAbsolutePath('@storybook/addon-links'),
+		getAbsolutePath('@storybook/addon-essentials'),
+		getAbsolutePath('@storybook/addon-interactions'),
 	],
 	framework: {
-		name: '@storybook/react-vite',
+		name: getAbsolutePath('@storybook/react-vite') as '@storybook/react-vite',
 		options: {},
 	},
 	docs: {
@@ -16,3 +17,7 @@ const config = {
 	},
 } satisfies StorybookConfig
 export default config
+
+function getAbsolutePath<T extends string>(value: T) {
+	return dirname(require.resolve(join(value, 'package.json')))
+}
