6c1d38ddab213096053c03c8cac02d2980180e31
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-02-09 11:47:15 +0100

    Add new dependencies and utilities function
    New dependencies 'class-variance-authority', 'tailwind-merge', and 'clsx' were added to the 'ui' package. Along with these, a utility function 'cn' was introduced in 'utils.ts', which utilizes the newly added dependencies. These changes help in enhancing the functionality of the 'ui' package.

---
+++ packages/ui/src/utils.ts
@@ -0,0 +1,6 @@
+import { twMerge } from 'tailwind-merge'
+import { clsx, type ClassValue } from 'clsx'
+
+export function cn<T extends ClassValue[]>(...inputs: T): string {
+	return twMerge(clsx(inputs))
+}
