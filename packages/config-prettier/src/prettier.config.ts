d53fd2f0b2b48eb2c868c3f8a49121474a9381ac
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-02-14 12:12:52 +0100

    Update config-prettier to use TypeScript
    The package config-prettier has been updated to use TypeScript. The existing JavaScript configuration file, prettier.config.mjs, has been deleted and replaced with a TypeScript file, prettier.config.ts. The package.json file has also been updated to reflect the use of TypeScript, with additional scripts for building and development.
    
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

---
+++ packages/config-prettier/src/prettier.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "prettier";
+
+const config = {
+  arrowParens: "avoid",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: "auto",
+  endOfLine: "lf",
+  experimentalTernaries: true,
+  jsxSingleQuote: false,
+  plugins: ["prettier-plugin-tailwindcss"],
+  printWidth: 100,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  semi: false,
+  singleAttributePerLine: true,
+  singleQuote: true,
+  tabWidth: 4,
+  trailingComma: "all",
+  useTabs: true,
+} satisfies Config;
+
+export default config;
