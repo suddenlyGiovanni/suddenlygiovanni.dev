a7bf0f6155122a0dfaddf1e64616d953b15e9d6f
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-02-23 11:37:10 +0100

    Add Contacts component to routes/resume
    A new Contacts component was created and added to the routes/resume to handle all contact related information. This helps to streamline the code in the main resume route by compartmentalizing the contact information into a separate component, improving code readability and maintainability.
    
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

---
+++ apps/web/app/routes/resume/contacts.tsx
@@ -0,0 +1,175 @@
+import { cn, Icons, SocialIcon, T } from '@suddenly-giovanni/ui'
+
+interface Location {
+	/**
+	 * To add multiple address lines, use
+	 * . For example, 1234 Glücklichkeit Straße
+	 * Hinterhaus 5. Etage li.
+	 */
+	address: string
+	city: string
+	/**
+	 * code as per ISO-3166-1 ALPHA-2, e.g. US, AU, IN
+	 */
+	countryCode: string
+	postalCode: string
+	/**
+	 * The general region where you live. Can be a US state, or a province, for instance.
+	 */
+	region: string
+}
+
+interface Basics {
+	/**
+	 * e.g. thomas@gmail.com
+	 */
+	email: string
+	/**
+	 * URL (as per RFC 3986) to a image in JPEG or PNG format
+	 */
+	image: string
+	/**
+	 * e.g. Web Developer
+	 */
+	label: string
+	location: Location
+	name: string
+	/**
+	 * Phone numbers are stored as strings so use any format you like, e.g. 712-117-2923
+	 */
+	phone: string
+	/**
+	 * Specify any number of social networks that you participate in
+	 */
+	profiles: Profile[]
+	/**
+	 * Write a short 2-3 sentence biography about yourself
+	 */
+	summary: string
+	/**
+	 * URL (as per RFC 3986) to your website, e.g. personal homepage
+	 */
+	url: string
+}
+
+interface Profile {
+	/**
+	 * e.g. Facebook or Twitter
+	 */
+	network: string
+	/**
+	 * e.g. http://twitter.example.com/neutralthoughts
+	 */
+	url: string
+	/**
+	 * e.g. neutralthoughts
+	 */
+	username: string
+}
+
+const addressClasses = {
+	address: cn(
+		[
+			'mx-auto',
+			'my-1',
+			'w-auto',
+			'max-w-max',
+			'rounded-md',
+			'bg-slate-500/15',
+			'px-24',
+			'py-1',
+		],
+		['md:w-full', 'md:max-w-none', 'md:px-0'],
+	),
+	ul: cn(
+		['ml-0', 'grid', 'grid-cols-1'],
+		['md:grid-cols-2', 'md:grid-flow-row-dense', 'md:justify-items-start'],
+	),
+	li: cn(
+		'flex',
+		'flex-row',
+		'items-center',
+		'gap-3',
+		'text-start',
+		'max-w-72',
+		'mx-auto',
+		'w-full',
+	),
+}
+
+export function Contacts({
+	email,
+	location,
+	phone,
+	profiles,
+	url,
+}: Pick<Basics, 'email' | 'location' | 'phone' | 'profiles' | 'url'>) {
+	return (
+		<address className={addressClasses.address}>
+			<T.ul className={addressClasses.ul}>
+				<li className={addressClasses.li}>
+					<Icons.globe
+						aria-label="location icon"
+						className="size-4"
+					/>
+					<T.a
+						href="https://www.openstreetmap.org/search?query=berlin#map=11/52.5072/13.4249"
+						target="_blank"
+					>{`${location.city}, ${location.countryCode}`}</T.a>
+				</li>
+
+				<li className={addressClasses.li}>
+					<Icons.envelope aria-label="mail icon" />
+					<T.a
+						aria-label="email"
+						href={`mailto:${email}`}
+						rel="noreferrer"
+						target="_blank"
+					>
+						{email}
+					</T.a>
+				</li>
+
+				<li className={addressClasses.li}>
+					<Icons.desktop aria-label="mail icon" />
+					<T.a
+						aria-label="link to my website"
+						href={url}
+						rel="noreferrer"
+						target="_blank"
+					>
+						{url}
+					</T.a>
+				</li>
+
+				<li className={addressClasses.li}>
+					<Icons.mobile aria-label="phone icon" />
+					<T.a
+						aria-label="phone number"
+						href={`tel:${phone || ''}`}
+					>
+						{phone}
+					</T.a>
+				</li>
+				{profiles.map((profile, idx) => (
+					<li
+						className={addressClasses.li}
+						key={String(idx) + String(profile.network)}
+					>
+						<SocialIcon
+							aria-label={`${profile.network || ''} icon`}
+							network={profile.network.toLowerCase()}
+						/>
+
+						<T.a
+							aria-label={`link to ${profile.network || ''}`}
+							href={profile.url}
+						>
+							{profile.url.replace(/(https:\/\/www\.)|(https:\/\/)/i, '')}
+						</T.a>
+					</li>
+				))}
+			</T.ul>
+		</address>
+	)
+}
--- apps/web/app/routes/resume/route.tsx
+++ apps/web/app/routes/resume/route.tsx
@@ -1,8 +1,9 @@
 import type { LinksFunction, MetaFunction } from '@remix-run/node'
 import { Link } from '@remix-run/react'
 import resume from '@suddenly-giovanni/resume'
-import { cn, Icons, SocialIcon, T } from '@suddenly-giovanni/ui'
+import { cn, T } from '@suddenly-giovanni/ui'
 import resumePdfAssetUrl from 'public/giovanni-ravalico-resume-2021.pdf?url'
+import { Contacts } from '~/routes/resume/contacts.tsx'
 import { Skills } from './skills.tsx'
 
 export const meta: MetaFunction = () => {
@@ -24,36 +25,6 @@ export const links: LinksFunction = () => {
 			href: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css',
 		},
 	]
-}
-
-const addressClasses = {
-	address: cn(
-		[
-			'mx-auto',
-			'my-1',
-			'w-auto',
-			'max-w-max',
-			'rounded-md',
-			'bg-slate-500/15',
-			'px-24',
-			'py-1',
-		],
-		['md:w-full', 'md:max-w-none', 'md:px-0'],
-	),
-	ul: cn(
-		['ml-0', 'grid', 'grid-cols-1'],
-		['md:grid-cols-2', 'md:grid-flow-row-dense', 'md:justify-items-start'],
-	),
-	li: cn(
-		'flex',
-		'flex-row',
-		'items-center',
-		'gap-3',
-		'text-start',
-		'max-w-72',
-		'mx-auto',
-		'w-full',
-	),
 }
 
 export default function Resume(): JSX.Element {
@@ -86,72 +57,13 @@ export default function Resume(): JSX.Element {
 					</em>
 				</T.p>
 
-				<address className={addressClasses.address}>
-					<T.ul className={addressClasses.ul}>
-						<li className={addressClasses.li}>
-							<Icons.globe
-								aria-label="location icon"
-								className="size-4"
-							/>
-							<T.a
-								href="https://www.openstreetmap.org/search?query=berlin#map=11/52.5072/13.4249"
-								target="_blank"
-							>{`${basics.location.city}, ${basics.location.countryCode}`}</T.a>
-						</li>
-
-						<li className={addressClasses.li}>
-							<Icons.envelope aria-label="mail icon" />
-							<T.a
-								aria-label="email"
-								href={`mailto:${basics.email}`}
-								rel="noreferrer"
-								target="_blank"
-							>
-								{basics.email}
-							</T.a>
-						</li>
-
-						<li className={addressClasses.li}>
-							<Icons.desktop aria-label="mail icon" />
-							<T.a
-								aria-label="link to my website"
-								href={basics.url}
-								rel="noreferrer"
-								target="_blank"
-							>
-								{basics.url}
-							</T.a>
-						</li>
-
-						<li className={addressClasses.li}>
-							<Icons.mobile aria-label="phone icon" />
-							<T.a
-								aria-label="phone number"
-								href={`tel:${basics.phone || ''}`}
-							>
-								{basics.phone}
-							</T.a>
-						</li>
-						{basics.profiles.map((profile, idx) => (
-							<li
-								className={addressClasses.li}
-								key={String(idx) + String(profile.network)}
-							>
-								<SocialIcon
-									aria-label={`${profile.network || ''} icon`}
-									network={profile.network.toLowerCase()}
-								/>
-
-								<T.a
-									aria-label={`link to ${profile.network || ''}`}
-									href={profile.url}
-								>
-									{profile.url.replace(/(https:\/\/www\.)|(https:\/\/)/i, '')}
-								</T.a>
-							</li>
-						))}
-					</T.ul>
-				</address>
+				<Contacts
+					email={basics.email}
+					location={basics.location}
+					phone={basics.phone}
+					profiles={basics.profiles}
+					url={basics.url}
+				/>
 
 				<T.p>
 					click on this link to download the pdf version of my resume{' '}
