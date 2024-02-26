c8d3655b344db0ef54c9a8e85d845df5ce8941dc
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-02-26 18:25:32 +0100

    Add styled examples and states to Collapsible component story
    This commit extends the story of Collapsible component by including styled examples covering different states. Each case is well illustrated using different classes and properties, providing a clear visual guide for different possible implementations and usage of the Collapsible component.
    
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

--- packages/ui/src/components/collapsible/collapsible.stories.tsx
+++ packages/ui/src/components/collapsible/collapsible.stories.tsx
@@ -1,9 +1,38 @@
-import * as React from 'react'
 import { CaretSortIcon } from '@radix-ui/react-icons'
-import type { Meta, StoryObj } from '@storybook/react'
-import type { ReactElement } from 'react'
+import type { Meta } from '@storybook/react'
+import { useState, type ReactElement } from 'react'
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '../../ui/collapsible'
 import { Button } from '../../ui/button'
+import { cn } from '../../lib/utils'
+
+const rootClass = cn('sans max-w-[20em]')
+
+const triggerClass = cn(
+	['w-full', 'text-center'],
+	'box-border border-0 bg-background p-2.5 text-xl text-foreground',
+	'focus:text-red-500 focus:shadow-inner focus:outline-none',
+	'data-[disabled]:text-gray-300',
+	'data-[state="open"]:bg-red-500 data-[state="open"]:text-white',
+	'focus:data-[state="open"]:text-black',
+)
+
+const contentClass = cn('p-2.5 leading-6')
+
+const styles = cn(
+	'border-2 border-blue-500 bg-blue-300 p-2.5',
+	'data-[state="closed"]:border-red-500',
+	'data-[state="open"]:border-green-500',
+	'data-[disabled]:border-dashed',
+	':disabled:opacity-50',
+)
+
+const rootAttrClass = cn(styles)
+const triggerAttrClass = cn(styles)
+const contentAttrClass = cn(
+	// ensure we can see the content (because it has `hidden` attribute)
+	'block',
+	styles,
+)
 
 const meta = {
 	title: 'Components/Collapsible',
@@ -12,7 +41,7 @@ const meta = {
 } satisfies Meta
 
 export function Default(): ReactElement {
-	const [isOpen, setIsOpen] = React.useState(false)
+	const [isOpen, setIsOpen] = useState(false)
 
 	return (
 		<Collapsible
@@ -46,5 +75,112 @@ export function Default(): ReactElement {
 		</Collapsible>
 	)
 }
+
+export function Styled(): ReactElement {
+	return (
+		<Collapsible className={rootClass}>
+			<CollapsibleTrigger className={triggerClass}>Trigger</CollapsibleTrigger>
+			<CollapsibleContent className={contentClass}>Content 1</CollapsibleContent>
+		</Collapsible>
+	)
+}
+
+export function Controlled(): ReactElement {
+	const [open, setOpen] = useState(false)
+	return (
+		<Collapsible
+			className={rootClass}
+			onOpenChange={setOpen}
+			open={open}
+		>
+			<CollapsibleTrigger className={triggerClass}>
+				{open ? 'close' : 'open'}
+			</CollapsibleTrigger>
+			<CollapsibleContent
+				asChild
+				className={contentClass}
+			>
+				<article>Content 1</article>
+			</CollapsibleContent>
+		</Collapsible>
+	)
+}
+
+export function Chromatic(): ReactElement {
+	return (
+		<>
+			<h1>Uncontrolled</h1>
+			<h2>Closed</h2>
+			<Collapsible className={rootClass}>
+				<CollapsibleTrigger className={triggerClass}>Trigger</CollapsibleTrigger>
+				<CollapsibleContent className={contentClass}>Content 1</CollapsibleContent>
+			</Collapsible>
+
+			<h2>Open</h2>
+			<Collapsible
+				className={rootClass}
+				defaultOpen
+			>
+				<CollapsibleTrigger className={triggerClass}>Trigger</CollapsibleTrigger>
+				<CollapsibleContent className={contentClass}>Content 1</CollapsibleContent>
+			</Collapsible>
+
+			<h1>Controlled</h1>
+			<h2>Closed</h2>
+			<Collapsible
+				className={rootClass}
+				open={false}
+			>
+				<CollapsibleTrigger className={triggerClass}>Trigger</CollapsibleTrigger>
+				<CollapsibleContent className={contentClass}>Content 1</CollapsibleContent>
+			</Collapsible>
+
+			<h2>Open</h2>
+			<Collapsible
+				className={rootClass}
+				open
+			>
+				<CollapsibleTrigger className={triggerClass}>Trigger</CollapsibleTrigger>
+				<CollapsibleContent className={contentClass}>Content 1</CollapsibleContent>
+			</Collapsible>
+
+			<h1>Disabled</h1>
+			<Collapsible
+				className={rootClass}
+				disabled
+			>
+				<CollapsibleTrigger className={triggerClass}>Trigger</CollapsibleTrigger>
+				<CollapsibleContent className={contentClass}>Content 1</CollapsibleContent>
+			</Collapsible>
+
+			<h1>State attributes</h1>
+			<h2>Closed</h2>
+			<Collapsible className={rootAttrClass}>
+				<CollapsibleTrigger className={triggerAttrClass}>Trigger</CollapsibleTrigger>
+				<CollapsibleContent className={contentAttrClass}>Content 1</CollapsibleContent>
+			</Collapsible>
+
+			<h2>Open</h2>
+			<Collapsible
+				className={rootAttrClass}
+				defaultOpen
+			>
+				<CollapsibleTrigger className={triggerAttrClass}>Trigger</CollapsibleTrigger>
+				<CollapsibleContent className={contentAttrClass}>Content 1</CollapsibleContent>
+			</Collapsible>
+
+			<h2>Disabled</h2>
+			<Collapsible
+				className={rootAttrClass}
+				defaultOpen
+				disabled
+			>
+				<CollapsibleTrigger className={triggerAttrClass}>Trigger</CollapsibleTrigger>
+				<CollapsibleContent className={contentAttrClass}>Content 1</CollapsibleContent>
+			</Collapsible>
+		</>
+	)
+}
+Chromatic.parameters = { chromatic: { disable: false } }
 
 export default meta
