f66f7df84b25e314974d42ffb9ac9ef7703e4bdf
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-02-26 16:43:58 +0100

    Add '@radix-ui/react-separator' to UI package
    A new dependency, '@radix-ui/react-separator' version 1.0.3, has been added to the UI package. This has also been added to the pnpm lock file and used to create a new Separator component in the '/ui' directory, providing increased functionality and styling options in the User Interface. This change exports the Separator in 'src/index.tsx'.
    
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -12,11 +12,11 @@ export {
 	SocialIcon,
 } from './components/index.tsx'
 export { Skeleton } from './ui/skeleton.tsx'
+export { Separator } from './ui/separator.tsx'
 export {
 	cn,
 	type PolymorphicRef,
 	type PolymorphicComponentPropWithRef,
 	type PolymorphicComponentProp,
 } from './lib'
-
 export { useToggle } from './hooks/use-toggle.tsx'
