af250935430f920f199c657a207a64895852ea0c
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-02-27 13:47:10 +0100

    Refactor Skills component in resume route
    The Skills component code in the resume route was restructured to improve readability and maintainability. Specific parts such as listing of keywords and individual keyword display have been separated into their own functional components. This helps in keeping the codebase modular and easier to manage.
    
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

--- apps/web/app/routes/resume/skills.tsx
+++ apps/web/app/routes/resume/skills.tsx
@@ -3,12 +3,16 @@ import { Option } from 'effect'
 import type { ReactElement } from 'react'
 import { getDevIconComponent } from './dev-icons.tsx'
 
-export function Skills(props: { skills: { name: string; keywords: string[] }[] }): ReactElement {
+export function Skills({
+	skills,
+}: {
+	skills: { name: string; keywords: string[] }[]
+}): ReactElement {
 	return (
 		<section>
 			<T.h2>Skills</T.h2>
 
-			{props.skills.map(({ name, keywords }) => (
+			{skills.map(({ name, keywords }) => (
 				<Skill
 					key={name}
 					keywords={keywords}
@@ -24,37 +28,48 @@ function Skill({ name, keywords }: { name: string; keywords: string[] }): ReactE
 		<dl key={name}>
 			<dt>{name}</dt>
 			<dd>
-				<ul
-					className={cn(
-						'my-0 ml-0 flex list-none flex-row flex-wrap items-start justify-start gap-x-4',
-					)}
-				>
-					{keywords.map(keyword => {
-						const maybeIcon = getDevIconComponent(keyword)
-						return (
-							<li
-								className={cn(
-									'my-0 flex w-fit flex-row items-center justify-start gap-1 align-middle',
-								)}
-								key={keyword}
-							>
-								{
-									// if it does not match Concepts or Methodologies...
-									Option.match(maybeIcon, {
-										onNone: () => keyword,
-										onSome: Icon => (
-											<>
-												<Icon className="size-4 fill-accent-foreground/80" />
-												{keyword}
-											</>
-										),
-									})
-								}
-							</li>
-						)
-					})}
-				</ul>
+				<KeywordsList keywords={keywords} />
 			</dd>
 		</dl>
+	)
+}
+
+function KeywordsList({ keywords }: { keywords: string[] }): ReactElement {
+	return (
+		<ul
+			className={cn(
+				'my-0 ml-0 flex list-none flex-row flex-wrap items-start justify-start gap-x-4',
+			)}
+		>
+			{keywords.map(keyword => (
+				<Keyword
+					key={keyword}
+					keyword={keyword}
+				/>
+			))}
+		</ul>
+	)
+}
+
+function Keyword({ keyword }: { keyword: string }): ReactElement {
+	const maybeIcon = getDevIconComponent(keyword)
+	return (
+		<li
+			className={cn('my-0 flex w-fit flex-row items-center justify-start gap-1 align-middle')}
+			key={keyword}
+		>
+			{
+				// if it does not match Concepts or Methodologies...
+				Option.match(maybeIcon, {
+					onNone: () => keyword,
+					onSome: Icon => (
+						<>
+							<Icon className="size-4 fill-accent-foreground/80" />
+							{keyword}
+						</>
+					),
+				})
+			}
+		</li>
 	)
 }
