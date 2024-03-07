165bb022e891255635554cfe60d22583c53f2098
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-03-07 01:03:55 +0100

    Refactor import statement in dropdown-menu.tsx
    The import path for the 'cn' utility function has been simplified. The path has been updated from '../lib/utils.ts' to '~/lib', enhancing the readability of the code and ensuring consistency in import statements.
    
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

--- packages/ui/src/ui/dropdown-menu.tsx
+++ packages/ui/src/ui/dropdown-menu.tsx
@@ -1,7 +1,7 @@
 import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu'
 import { CheckIcon, ChevronRightIcon, DotFilledIcon } from '@radix-ui/react-icons'
 import * as React from 'react'
-import { cn } from '../lib/utils.ts'
+import { cn } from '~/lib'
 
 const DropdownMenu = DropdownMenuPrimitive.Root
 
