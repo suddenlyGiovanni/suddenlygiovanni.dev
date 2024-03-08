e9969412f7d1a86c2a343f00b952240fd2233d67
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-03-08 20:58:36 +0100

    Refactor conditional rendering in resume pages
    This commit refactors the conditional rendering syntax in `experiences
    .tsx` and `education.tsx`. Specifically, it replaces the previous
    pattern with a ternary pattern for cleaner and more readable code. It
    also modifies the `tsconfig.json` and `env.d.ts` files to better
    organize TypeScript configuration and type declarations.
    
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

--- apps/web/env.d.ts
+++ apps/web/env.d.ts
@@ -1,8 +1,3 @@
-/// <reference types="@remix-run/node" />
-/// <reference types="vite/client" />
-/// <reference types="@total-typescript/ts-reset" />
-import type { ReactElement } from 'react'
-
 declare module '*.mdx' {
 	let MDXComponent: (props: unknown) => ReactElement
 	export const frontmatter: unknown
