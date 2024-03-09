48211e01b78b9ea661b498292a389838f15a7fe0
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-03-09 01:51:38 +0100

    feat(ui): adjust checkbox behavior in dropdown-menu
    This patch uses a conditional operator for the 'checked' prop in the
    DropdownMenuPrimitive.CheckboxItem component. The update will ensure
    that the 'checked' attribute is only applied when it's true, improving
    performance and rendering logic in the dropdown menu.
    
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

--- packages/ui/src/ui/dropdown-menu.tsx
+++ packages/ui/src/ui/dropdown-menu.tsx
@@ -93,7 +93,7 @@ const DropdownMenuCheckboxItem = React.forwardRef<
 	React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.CheckboxItem>
 >(({ className, children, checked, ...props }, ref) => (
 	<DropdownMenuPrimitive.CheckboxItem
-		checked={checked}
+		{...(checked ? { checked } : {})}
 		className={cn(
 			'relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
 			className,
