704fa4ea3fdc716a65ecda858fad4e04cc77683b
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-03-11 01:57:57 +0100

    feat(web-app): extend Education interface in resume route
    Expanded the Education interface within the resume route of the web app
    module. The updated interface now includes optional string fields for
    `area`, `courses`, `endDate`, `gpa`, `institution`, `startDate`,
    `studyType`, `url`, and `location` providing a broader range of data for
     user's education related details.
    
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

--- apps/web/app/routes/resume/interface.ts
+++ apps/web/app/routes/resume/interface.ts
@@ -274,7 +274,71 @@ const Basics = S.struct({
 
 export interface Basics extends S.Schema.To<typeof Basics> {}
 
-export interface Education {
+const Education = S.struct({
+	/**
+	 * e.g. Arts
+	 */
+	area: S.optional(S.string, {
+		exact: true,
+		annotations: {
+			title: 'area',
+			description: 'e.g. Arts',
+			examples: ['Arts'],
+		},
+	}),
+
+	courses: S.optional(S.array(S.string), {
+		exact: true,
+		annotations: { title: 'courses', description: 'List notable courses/subjects' },
+	}),
+
+	endDate: S.optional(ISODateString),
+
+	gpa: S.optional(S.string, {
+		exact: true,
+		annotations: {
+			title: 'gpa',
+			description: 'grade point average, e.g. 3.67/4.0',
+			examples: ['3.67/4.0'],
+		},
+	}),
+
+	institution: S.optional(S.string, {
+		annotations: {
+			title: 'institution',
+			description: 'e.g. Massachusetts Institute of Technology',
+			examples: ['Massachusetts Institute of Technology'],
+		},
+	}),
+
+	startDate: S.optional(ISODateString),
+
+	/**
+	 * e.g. Bachelor
+	 */
+	studyType: S.optional(S.string, {
+		exact: true,
+		annotations: {
+			title: 'studyType',
+			description: 'tuYpe of study',
+			examples: ['Bachelor'],
+		},
+	}),
+
+	/**
+	 * e.g. http://facebook.example.com
+	 */
+	url: S.optional(UrlString, {
+		exact: true,
+		annotations: {
+			title: 'url',
+			description: 'URL (as per RFC 3986)',
+		},
+	}),
+	location: S.optional(S.string),
+})
+
+export interface Education extends S.Schema.To<typeof Education> {
 	/**
 	 * e.g. Arts
 	 */
