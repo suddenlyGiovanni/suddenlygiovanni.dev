0fd2081bd13295be7d22d60ceead99b9ffc95100
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-03-11 15:44:40 +0100

    feat(web-resume): export types from interface and add tests
    Made ISODateString and Award types exportable in resume interface.ts.
    Added a new test file iso-date-string.spec.ts for validating ISO date
    string schema.
    
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

--- apps/web/app/routes/resume/interface.ts
+++ apps/web/app/routes/resume/interface.ts
@@ -13,13 +13,13 @@ const UrlString: S.Schema<string> = S.string.pipe(
 	S.examples(['https://facebook.example.com']),
 )
 
-const ISODateString = S.string.pipe(
+export const ISODateString = S.string.pipe(
 	S.pattern(
 		/^[0-9]{4}-((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01])|(0[469]|11)-(0[1-9]|[12][0-9]|30)|(02)-(0[1-9]|[12][0-9]))T(0[0-9]|1[0-9]|2[0-3]):(0[0-9]|[1-5][0-9]):(0[0-9]|[1-5][0-9])\.[0-9]{3}Z$/,
 		{
 			title: 'date',
 			description: 'Using ISO 8601',
-			examples: ['2012-04-05', '2012-04-05T10:00:00.000Z'],
+			examples: ['2012-04-05T10:00:00.000Z'],
 		},
 	),
 )
@@ -32,7 +32,7 @@ const Email = S.string.pipe(
 	}),
 )
 
-const Award = S.struct({
+export const Award = S.struct({
 	awarder: S.optional(S.string.pipe(S.trimmed(), S.nonEmpty()), {
 		exact: true,
 		annotations: {
@@ -316,6 +316,7 @@ const Interest = S.struct({
 		},
 	}),
 })
+
 export interface Interest extends S.Schema.To<typeof Interest> {}
 
 const Language = S.struct({
@@ -540,6 +541,7 @@ const Skill = S.struct({
 		S.examples(['Web Development']),
 	),
 })
+
 export interface Skill extends S.Schema.To<typeof Skill> {}
 
 const Volunteer = S.struct({
