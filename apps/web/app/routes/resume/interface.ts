0186581da076c81b8d48f99d431c884e9ea9e6b5
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-03-11 14:14:32 +0100

    feat(resume): refactor ISODateString and Email schemas
    Change ISODateString and Email schemas in 'resume' route to use the pipe
     method. The pipe method provides better maintainability and scale in
     validation schemas. This update should have no impact on functionality,
      but enhances code organization.
    
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

--- apps/web/app/routes/resume/interface.ts
+++ apps/web/app/routes/resume/interface.ts
@@ -13,22 +13,23 @@ const UrlString: S.Schema<string> = S.string.pipe(
 	S.examples(['https://facebook.example.com']),
 )
 
-const ISODateString = S.pattern(
-	/^[0-9]{4}-((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01])|(0[469]|11)-(0[1-9]|[12][0-9]|30)|(02)-(0[1-9]|[12][0-9]))T(0[0-9]|1[0-9]|2[0-3]):(0[0-9]|[1-5][0-9]):(0[0-9]|[1-5][0-9])\.[0-9]{3}Z$/,
-	{
-		title: 'date',
-		description: 'Using ISO 8601',
-		examples: ['2012-04-05', '2012-04-05T10:00:00.000Z'],
-	},
+const ISODateString = S.string.pipe(
+	S.pattern(
+		/^[0-9]{4}-((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01])|(0[469]|11)-(0[1-9]|[12][0-9]|30)|(02)-(0[1-9]|[12][0-9]))T(0[0-9]|1[0-9]|2[0-3]):(0[0-9]|[1-5][0-9]):(0[0-9]|[1-5][0-9])\.[0-9]{3}Z$/,
+		{
+			title: 'date',
+			description: 'Using ISO 8601',
+			examples: ['2012-04-05', '2012-04-05T10:00:00.000Z'],
+		},
+	),
 )
 
-const Email = S.pattern(
-	/^(?!\.)(?!.*\.\.)([A-Z0-9_+-.]*)[A-Z0-9_+-]@([A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i,
-	{
+const Email = S.string.pipe(
+	S.pattern(/^(?!\.)(?!.*\.\.)([A-Z0-9_+-.]*)[A-Z0-9_+-]@([A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i, {
 		title: 'email',
 		description: 'Email address',
 		examples: ['foop@bar.baz'],
-	},
+	}),
 )
 
 const Award = S.struct({
