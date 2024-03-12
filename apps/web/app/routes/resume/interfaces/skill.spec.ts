880648ac868add8ed036ad91facbbb463531a93f
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-03-12 04:03:16 +0100

    test(web): add Skill interface unit tests
    Add a suite of unit tests for the Skill interface within the web
    application's resume module. Ensure input validation for Skill
    properties such as keywords, level, and name using the decode
    functionality from '@effect/schema/Schema'.
    
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

---
+++ apps/web/app/routes/resume/interfaces/skill.spec.ts
@@ -0,0 +1,38 @@
+import * as S from '@effect/schema/Schema'
+import { expect, test, describe } from 'vitest'
+import { Skill } from './skill.ts'
+
+describe('Skill', () => {
+	const skillInput = {
+		keywords: ['Rust', 'Java', 'Scala'],
+		level: 'Wizard',
+		name: 'Web Development',
+	} satisfies S.Schema.To<typeof Skill>
+
+	describe('decode', () => {
+		const parse = S.decodeUnknownSync(Skill)
+
+		test('handle missing partial properties', () => {
+			expect(() => parse({ name: skillInput.name })).not.toThrow()
+		})
+
+		test('keywords', () => {
+			expect(() => parse({ name: skillInput.name, keywords: [] })).not.toThrow()
+			expect(() => parse({ name: skillInput.name, keywords: [''] })).toThrow()
+			expect(() => parse({ name: skillInput.name, keywords: [' '] })).toThrow()
+			expect(() => parse({ name: skillInput.name, keywords: skillInput.keywords })).not.toThrow()
+		})
+
+		test('level', () => {
+			expect(() => parse({ name: skillInput.name, level: '' })).toThrow()
+			expect(() => parse({ name: skillInput.name, level: ' ' })).toThrow()
+			expect(() => parse({ name: skillInput.name, level: skillInput.level })).not.toThrow()
+		})
+
+		test('name', () => {
+			expect(() => parse({ name: '' })).toThrow()
+			expect(() => parse({ name: ' ' })).toThrow()
+			expect(() => parse({ name: skillInput.name })).not.toThrow()
+		})
+	})
+})
