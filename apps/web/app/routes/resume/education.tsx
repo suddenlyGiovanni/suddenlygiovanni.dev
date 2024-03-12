33bb5b70d9c381e6bcb58ee436148646b3cf8688
suddenlyGiovanni <suddenlyGiovanni@users.noreply.github.com>
2024-03-12 00:18:42 +0000

    chore: lint code with Biome [skip ci]

--- apps/web/app/routes/resume/education.tsx
+++ apps/web/app/routes/resume/education.tsx
@@ -11,7 +11,7 @@ import { Button } from '@suddenly-giovanni/ui/ui/button.tsx'
 import type { ReactElement } from 'react'
 import { memo, useCallback, useMemo, useState } from 'react'
 
-import {Education as IEducation} from '~/routes/resume/interfaces/education.ts'
+import { Education as IEducation } from '~/routes/resume/interfaces/education.ts'
 
 function formatDateLocaleShort(date: Date): string {
 	return date.toLocaleDateString('en-US', {
