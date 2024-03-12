1b23ae6a52c490644c7983a568db069139ef7a76
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-03-12 01:03:26 +0100

    refactor(web): extract Project interface to own file
    Move the Project schema definition from the interfaces module to a
    separate file to enhance modularity and improve code organization.
    The Project schema is now independently maintained, ensuring cleaner and
    more focused interface definitions.
    
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

--- apps/web/app/routes/resume/interfaces/interface.ts
+++ apps/web/app/routes/resume/interfaces/interface.ts
@@ -1,6 +1,6 @@
 import * as S from '@effect/schema/Schema'
 
-import { Meta } from './meta.ts'
+import { Project } from './project.ts'
 import { Award } from './award.ts'
 import { Basics } from './basics.ts'
 import { Education } from './education.ts'
@@ -8,70 +8,8 @@ import { Email } from './email.ts'
 import { Interest } from './interest.ts'
 import { ISODateString } from './iso-date-string.ts'
 import { Language } from './language.ts'
+import { Meta } from './meta.ts'
 import { UrlString } from './url-string.ts'
-
-const Project = S.partial(
-	S.struct({
-		description: S.string.pipe(
-			S.trimmed(),
-			S.nonEmpty(),
-			S.description('Short summary of project'),
-			S.examples(['Collated works of 2017']),
-		),
-
-		endDate: ISODateString,
-
-		entity: S.string.pipe(
-			S.trimmed(),
-			S.nonEmpty(),
-			S.description('Specify the relevant company/entity affiliations'),
-			S.examples(['greenpeace', 'corporationXYZ']),
-		),
-
-		highlights: S.array(S.string.pipe(S.trimmed(), S.nonEmpty())).pipe(
-			S.title('highlights'),
-			S.description('Specify multiple features'),
-			S.examples(['Feature 1']),
-		),
-
-		keywords: S.array(S.string.pipe(S.trimmed(), S.nonEmpty())).pipe(
-			S.title('keywords'),
-			S.description('Specify special elements involved'),
-			S.examples(['special', 'elements']),
-		),
-
-		name: S.string.pipe(
-			S.trimmed(),
-			S.nonEmpty(),
-			S.description('Name of the project'),
-			S.examples(['The World Wide Web']),
-		),
-
-		roles: S.array(S.string.pipe(S.trimmed(), S.nonEmpty())).pipe(
-			S.title('roles'),
-			S.description('Specify your role on this project or in company'),
-			S.examples(['Software Engineer Lead']),
-		),
-
-		startDate: ISODateString,
-
-		type: S.string.pipe(
-			S.trimmed(),
-			S.nonEmpty(),
-			S.title('type'),
-			S.description('Type of project'),
-			S.examples(['volunteering', 'presentation', 'talk', 'application', 'conference']),
-		),
-
-		url: UrlString.pipe(
-			S.title('url'),
-			S.description('URL (as per RFC 3986)'),
-			S.examples(['http://www.computer.org.csdl/mags/co/1996/10/rx069-abs.html']),
-		),
-	}),
-)
-
-export interface Project extends S.Schema.To<typeof Project> {}
 
 const Publication = S.struct({
 	name: S.optional(S.string, {
