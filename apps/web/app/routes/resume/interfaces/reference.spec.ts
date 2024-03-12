e7a31aca7b9757f1e1706a11ccf62716fe9f95b6
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-03-12 03:49:18 +0100

    feat(web:resume): add Reference schema tests
    Introduced test suite for the Reference schema in the resume module. The
    new tests validate non-empty name and reference fields.
    
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

---
+++ apps/web/app/routes/resume/interfaces/reference.spec.ts
@@ -0,0 +1,29 @@
+import * as S from '@effect/schema/Schema'
+import { expect, test, describe } from 'vitest'
+import { Reference } from './reference.ts'
+
+describe('Reference', () => {
+	const referenceInput = {
+		name: 'Timothy Cook',
+		reference:
+			'Joe blogs was a great employee, who turned up to work at least once a week. He exceeded my expectations when it came to doing nothing.',
+	} satisfies S.Schema.To<typeof Reference>
+
+	describe('decode', () => {
+		const parse = S.decodeUnknownSync(Reference)
+
+		test('name', () => {
+			expect(() => parse({ name: '' })).toThrow()
+			expect(() => parse({ name: '  ' })).toThrow()
+			expect(() => parse({ name: referenceInput.name })).not.toThrow()
+		})
+
+		test('reference', () => {
+			expect(() => parse({ name: referenceInput.name, reference: '' })).toThrow()
+			expect(() => parse({ name: referenceInput.name, reference: '  ' })).toThrow()
+			expect(() =>
+				parse({ name: referenceInput.name, reference: referenceInput.reference }),
+			).not.toThrow()
+		})
+	})
+})
