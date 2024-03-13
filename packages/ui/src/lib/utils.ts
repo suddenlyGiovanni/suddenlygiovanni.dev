6531e54bf284a9f7ac4b73f8c7b6074f18e054f6
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-03-13 12:48:26 +0100

    chore(ui-utils): add barrel file for exports
    Introduced a barrel file mechanism with comments to bypass linting rules
    . This centralizes re-exporting utility functions from external
    libraries, improving import efficiency.
    
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

--- packages/ui/src/lib/utils.ts
+++ packages/ui/src/lib/utils.ts
@@ -5,5 +5,6 @@ export function clsx<T extends ClassValue[]>(...inputs: T): string {
 	return twMerge(_clsx(inputs))
 }
 
+// biome-ignore lint/nursery/noBarrelFile: need a barrel file
 export { type VariantProps, cva } from 'class-variance-authority'
 export { tv } from 'tailwind-variants'
