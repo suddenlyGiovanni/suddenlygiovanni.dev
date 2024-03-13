0e9308f175c8baacc9271afb9dbc7c0698e9118b
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-03-13 13:26:17 +0100

    feat(web): enable Codecov integration
    Enable conditional Codecov bundle analysis and token upload in the web
    Vite configuration. Linter-specific comments have been added to bypass
    complexity checks provided by biome linter annotations.
    
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -28,11 +28,15 @@ export default defineConfig({
 		tsconfigPaths(),
 		codecovVitePlugin({
 			bundleName: 'web',
+			// biome-ignore lint/complexity/useLiteralKeys: TS4111: Property 'CODECOV_TOKEN' comes from an index signature, so it must be accessed with ['CODECOV_TOKEN'].
 			enableBundleAnalysis: process.env['CODECOV_TOKEN'] !== undefined,
+			// biome-ignore lint/style/noNonNullAssertion: it is fine to be undefined or sting
+			// biome-ignore lint/complexity/useLiteralKeys: TS4111: Property 'CODECOV_TOKEN' comes from an index signature, so it must be accessed with ['CODECOV_TOKEN'].
 			uploadToken: process.env['CODECOV_TOKEN']!,
 		}),
 	],
 	test: {
+		// biome-ignore lint/complexity/useLiteralKeys: TS4111: Property 'CODECOV_TOKEN' comes from an index signature, so it must be accessed with ['CODECOV_TOKEN'].
 		reporters: process.env['GITHUB_ACTIONS'] ? ['dot', 'github-actions'] : ['default'],
 		coverage: {
 			provider: 'v8',
