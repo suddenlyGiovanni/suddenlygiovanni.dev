c0e3461957a095c9f27dc4a5bceb58f3328dc173
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-03-16 02:33:35 +0100

    refactor(web): update skill schema annotations
    Simplify annotation usage in the skill schema by adopting the
    `annotations` method over the `pipe` for a more streamlined codebase.
    This change enhances readability and maintenance of schema definitions
    within the resume module.
    
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

--- apps/web/app/routes/resume/schema/skill.ts
+++ apps/web/app/routes/resume/schema/skill.ts
@@ -2,26 +2,31 @@
 import * as S from '@effect/schema/Schema'
 
 export const Skill = S.struct({
-	keywords: S.array(S.compose(S.Trim, S.NonEmpty)).pipe(
-		S.title('keywords'),
-		S.description('List some keywords pertaining to this skill'),
-		S.examples([['Rust', 'Java']]),
-	),
+	keywords: S.array(
+		S.compose(S.Trim, S.NonEmpty).annotations({
+			title: 'keyword',
+			examples: ['Rust'],
+		}),
+	).annotations({
+		title: 'keywords',
+		description: 'List some keywords pertaining to this skill',
+		examples: [['Rust', 'Java']],
+	}),
 
 	level: S.optional(
-		S.compose(S.Trim, S.NonEmpty).pipe(
-			S.title('level'),
-			S.description('Level of expertise'),
-			S.examples(['Master', 'Intermediate']),
-		),
+		S.compose(S.Trim, S.NonEmpty).annotations({
+			title: 'level',
+			description: 'Level of expertise',
+			examples: ['Master', 'Intermediate'],
+		}),
 		{ exact: true },
 	),
 
-	name: S.compose(S.Trim, S.NonEmpty).pipe(
-		S.title('name'),
-		S.description('Name of the skill'),
-		S.examples(['Web Development']),
-	),
+	name: S.compose(S.Trim, S.NonEmpty).annotations({
+		title: 'name',
+		description: 'Name of the skill',
+		examples: ['Web Development'],
+	}),
 })
 
 export type Skill = S.Schema.Encoded<typeof Skill>
