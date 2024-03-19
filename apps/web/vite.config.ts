030aae6872624fd3f35fcce343f719cd06e52d80
suddenlyGiovanni <suddenlyGiovanni@users.noreply.github.com>
2024-03-19 16:33:46 +0000

    chore: lint code with Biome [skip ci]

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,9 +4,9 @@ import { vitePlugin as remix } from '@remix-run/dev'
 import { installGlobals } from '@remix-run/node'
 import remarkFrontmatter from 'remark-frontmatter'
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
+import { remixDevTools } from 'remix-development-tools'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { remixDevTools } from "remix-development-tools";
 
 installGlobals()
 
