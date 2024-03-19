e376bf2453c21e1b9b92256fd163a01448b69945
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-03-19 01:33:46 +0100

    refactor(web): improve formatDateLocaleShort function
    Refactored formatDateLocaleShort to utilize functional composition and
    updated the corresponding unit tests. Enhanced code readability and
    maintainability by using pipeline operator and more specific imports.
    
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

--- apps/web/app/routes/resume/format-date-locale-short.ts
+++ apps/web/app/routes/resume/format-date-locale-short.ts
@@ -1,20 +1,29 @@
 import type { ParseError } from '@effect/schema/ParseResult'
-import * as Schema from '@effect/schema/Schema'
+import { transform, string, Date as $Date, decodeEither } from '@effect/schema/Schema'
 import type { Either } from 'effect/Either'
+import { pipe } from 'effect/Function'
 
+/**
+ * Given a string date, this function validates and transforms it to a US locale short date format.
+ *
+ * @param isoString - The input string date in ISO format.
+ * @returns Either the formatted US locale short date or a parse error.
+ */
 export function formatDateLocaleShort(isoString: string): Either<string, ParseError> {
-	// TODO: missing tsdocs; starting from a string date, validate it to be valid date, then transform it to a us locale short date
-	const localDateSchema: Schema.Schema<string> = Schema.Date.pipe(
-		Schema.transform(
-			Schema.string,
-			fromDate => fromDate.toLocaleDateString('en-US', { month: 'short', year: 'numeric' }),
-			fromLocaleDateString => new Date(fromLocaleDateString),
-		),
-	).annotations({
-		title: 'localDate',
-		description: 'a short US locale date format',
-		examples: ['Apr 2022'],
-	})
-	const parse = Schema.decodeEither(localDateSchema)
-	return parse(isoString)
+	return pipe(
+		$Date
+			.pipe(
+				transform(
+					string,
+					fromDate => fromDate.toLocaleDateString('en-US', { month: 'short', year: 'numeric' }),
+					fromLocaleDateString => new Date(fromLocaleDateString),
+				),
+			)
+			.annotations({
+				title: 'localDate',
+				description: 'a short US locale date format',
+				examples: ['Apr 2022'],
+			}),
+		decodeEither,
+	)(isoString)
 }
