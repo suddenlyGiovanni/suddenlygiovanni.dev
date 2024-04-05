8540b8d9d447c2869deadb017186ad07f07e72b5
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-04-05 18:38:40 +0200

    feat(web app): initialize environment in entry.server.tsx
    The code has been updated to initialize the environment variables in
    entry.server.tsx. This involved importing functions from the env.server
    .ts utility and executing them to set up global environment variables.
    
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

--- apps/web/app/entry.server.tsx
+++ apps/web/app/entry.server.tsx
@@ -5,7 +5,12 @@ import { RemixServer } from '@remix-run/react'
 import { isbot } from 'isbot'
 import { renderToPipeableStream } from 'react-dom/server'
 
+import { getEnv, init } from '~/utils/env.server.ts'
+
 const ABORT_DELAY = 5_000
+
+init()
+global.ENV = getEnv()
 
 export default function handleRequest(
 	request: Request,
