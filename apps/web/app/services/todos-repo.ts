9605793e5f455381449a21660de7710dcf5954a4
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-04-29 23:47:09 +0200

    Effectify Remix (#838)
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
    Co-authored-by: suddenlyGiovanni <suddenlyGiovanni@users.noreply.github.com>

---
+++ apps/web/app/services/todos-repo.ts
@@ -0,0 +1,22 @@
+import { Effect, Layer } from 'effect'
+
+interface Todo {
+	readonly id: number
+	readonly title: string
+	readonly createdAt: string
+	readonly status: 'COMPLETER' | 'CREATED'
+}
+
+const makeTodoRepo = Effect.sync(() => {
+	return {
+		getAllTodos: Effect.succeed<Todo[]>([]),
+	}
+})
+
+// biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
+export class TodoRepo extends Effect.Tag('@services/TodoRepo')<
+	TodoRepo,
+	Effect.Effect.Success<typeof makeTodoRepo>
+>() {
+	static Live = Layer.effect(this, makeTodoRepo)
+}
