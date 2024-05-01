825c50741be8652741a62ceab76c927a54ba28a1
suddenlyGiovanni <suddenlyGiovanni@users.noreply.github.com>
2024-05-01 09:29:20 +0000

    chore: lint code with Biome [skip ci]

--- apps/web/app/services/index.ts
+++ apps/web/app/services/index.ts
@@ -1,8 +1,8 @@
 import { Layer } from 'effect'
 import { makeRemixRuntime } from '~/lib/utils.ts'
+import { OctokitService } from '~/services/octokit.ts'
 import { ResumeRepository } from '~/services/resume-repository.ts'
 import { TodoRepo } from '~/services/todos-repo.ts'
-import { OctokitService } from '~/services/octokit.ts'
 
 export const { loaderFunction } = makeRemixRuntime(
 	Layer.mergeAll(TodoRepo.Live, OctokitService.Live, ResumeRepository.Live),
