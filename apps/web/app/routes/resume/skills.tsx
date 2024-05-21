4e41c02bf0fa77832e0f540881dc0453a83ce64e
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-05-21 13:00:47 +0200

    Apps Web / Fix CSS class names in skills.tsx (#893)
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

--- apps/web/app/routes/resume/skills.tsx
+++ apps/web/app/routes/resume/skills.tsx
@@ -38,7 +38,7 @@ export const Skills = memo(function Skills({
 			<T.h2>Skills</T.h2>
 
 			<Button
-				className="absolute·top-0·right-0·rounded-full"
+				className="absolute top-0 right-0 rounded-full"
 				onClick={toggleSkillsAccordion}
 				size="icon"
 				variant="ghost"
