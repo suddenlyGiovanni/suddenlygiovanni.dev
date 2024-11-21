8c7fc42c948b4fd3870ff1483a5c8081817fffa2
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-11-21 22:04:16 +0100

    refactor(web): update routes configuration
    Refactored the routes configuration to improve readability and
    maintainability. Replaced the flatRoutes function with explicit route
     definitions using the route and index functions from
     @remix-run/route-config.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

--- apps/web/app/routes.ts
+++ apps/web/app/routes.ts
@@ -1,4 +1,10 @@
-import { flatRoutes } from '@remix-run/fs-routes'
-import type { RouteConfig } from '@remix-run/route-config'
+import { type RouteConfig, index, route } from '@remix-run/route-config'
 
-export const routes: RouteConfig = flatRoutes()
+export const routes = [
+	index('routes/_index.tsx'),
+	route('/motivations', 'routes/motivations/route.tsx'),
+	route('/brain-dump', 'routes/brain-dump/route.tsx'),
+	route('/reading-journal', 'routes/reading-journal/route.tsx'),
+	route('/resume', 'routes/resume/route.tsx'),
+	route('/resources/healthcheck', 'routes/resources.healthcheck.tsx'),
+] satisfies RouteConfig
