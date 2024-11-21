da7b71705e09385b8744f856a67ab7ab0430cefe
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-11-21 20:56:52 +0100

    refactor(web): remove unnecessary global installations
    The installation of globals is no longer necessary in the configuration. This change simplifies the vite.config.ts file and ensures cleaner setup.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,11 +1,8 @@
 import { codecovVitePlugin } from '@codecov/vite-plugin'
 import { vitePlugin as remix } from '@remix-run/dev'
-import { installGlobals } from '@remix-run/node'
 import { remixDevTools } from 'remix-development-tools'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-
-installGlobals()
 
 /// <reference types="vitest" />
 
