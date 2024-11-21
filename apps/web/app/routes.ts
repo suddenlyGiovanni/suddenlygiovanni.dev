6b532b876833913ab7401dbcd91085d6fd8adcb5
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-11-21 23:19:18 +0100

    refactor(routes): rename resume route file
    Renamed 'route.tsx' to 'resume.tsx' for better clarity and consistency. This change ensures that file names are more descriptive of their contents and avoids potential confusion.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

--- apps/web/app/routes.ts
+++ apps/web/app/routes.ts
@@ -5,7 +5,7 @@ export const routes = [
 	route('/motivations', 'routes/motivations.tsx'),
 	route('/second-brain', 'routes/second-brain.tsx'),
 	route('/reading-journal', 'routes/reading-journal.tsx'),
-	route('/resume', 'routes/resume/route.tsx'),
+	route('/resume', 'routes/resume/resume.tsx'),
 	...prefix('/resources', [
 		//
 		route('/healthcheck', 'routes/resources.healthcheck.tsx'),
