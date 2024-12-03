40ca6a62e22f64be77a3baed9b769972a0b15522
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-12-03 22:48:32 +0100

    feat(ui): integrate custom TailwindCSS setup
    Remove dependency on shared Tailwind configuration and set up custom
    TailwindCSS styles, themes, and plugins. Directly manage typography
    and animation plugins to enhance styling capabilities. This change
    allows greater flexibility and independence in styling configurations.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

--- packages/ui/postcss.config.mjs
+++ packages/ui/postcss.config.mjs
@@ -1,11 +1,10 @@
-import sharedPostcssConfig from '@suddenlygiovanni/config-tailwind/postcss.config.js'
-
 /**
  * If you want to use other PostCSS plugins, see the following:
  * https://tailwindcss.com/docs/using-with-preprocessors
  */
-/**  @type {import('@suddenlygiovanni/config-tailwind/postcss.config.js').Config} */
-// biome-ignore lint/style/noDefaultExport: This is a configuration file
 export default {
-	...sharedPostcssConfig,
+	plugins: {
+		tailwindcss: {},
+		autoprefixer: {},
+	},
 }
--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,10 +1,14 @@
+import { resolve } from 'node:path'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config/
-// biome-ignore lint/style/noDefaultExport: this is how Vite expects the config to be
 export default defineConfig({
+	resolve: {
+		alias: {
+			'@fonts': resolve('../../apps/web/public/fonts'),
+		},
+	},
 	build: {
 		target: 'esnext',
 	},
