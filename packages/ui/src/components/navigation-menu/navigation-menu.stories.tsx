48c03a915d661c2e525646852af42176f3c445eb
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-12-04 00:28:23 +0100

    refactor(ui): simplify className usage in navigation-menu
    Condense multiple className strings into single-line format in the
    NavigationMenu. This change enhances readability and reduces code
    redundancy without altering the component layout or behavior.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

--- packages/ui/src/components/navigation-menu/navigation-menu.stories.tsx
+++ packages/ui/src/components/navigation-menu/navigation-menu.stories.tsx
@@ -64,33 +64,13 @@ export function Default(): ReactElement {
 					<NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
 					<NavigationMenuContent>
 						<ul
-							className={clsx(
-								'grid',
-								'gap-3',
-								'p-4',
-								'md:w-[400px]',
-								'lg:w-[500px]',
-								'lg:grid-cols-[.75fr_1fr]',
-							)}
+							className={clsx('grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]')}
 						>
 							<li className="row-span-3">
 								<NavigationMenuLink asChild={true}>
 									<a
 										className={clsx(
-											'flex',
-											'h-full',
-											'w-full',
-											'select-none',
-											'flex-col',
-											'justify-end',
-											'rounded-md',
-											'bg-gradient-to-b',
-											'from-muted/50',
-											'to-muted',
-											'p-6',
-											'no-underline',
-											'outline-none',
-											'focus:shadow-md',
+											'flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md',
 										)}
 										href="/"
 									>
@@ -127,15 +107,7 @@ export function Default(): ReactElement {
 					<NavigationMenuTrigger>Components</NavigationMenuTrigger>
 					<NavigationMenuContent>
 						<ul
-							className={clsx(
-								'grid',
-								'w-[400px]',
-								'gap-3',
-								'p-4',
-								'md:w-[500px]',
-								'md:grid-cols-2',
-								'lg:w-[600px]',
-							)}
+							className={clsx('grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]')}
 						>
 							{components.map(component => (
 								<ListItem
@@ -168,19 +140,7 @@ const ListItem = forwardRef<ElementRef<'a'>, ComponentPropsWithoutRef<'a'>>(
 				<NavigationMenuLink asChild={true}>
 					<a
 						className={clsx(
-							'block',
-							'select-none',
-							'space-y-1',
-							'rounded-md',
-							'p-3',
-							'leading-none',
-							'no-underline',
-							'outline-none',
-							'transition-colors',
-							'hover:bg-accent',
-							'hover:text-accent-foreground',
-							'focus:bg-accent',
-							'focus:text-accent-foreground',
+							'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
 							className,
 						)}
 						ref={ref}
