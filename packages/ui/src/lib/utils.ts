8a8edb519994b6ebfeb0ed28abd0172e6c1b5cdf
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-12-03 21:48:36 +0100

    feat(ui): replace `clsx` with custom scoped package (#1077)
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

--- packages/ui/src/lib/utils.ts
+++ packages/ui/src/lib/utils.ts
@@ -1,8 +1,14 @@
-import { type ClassValue, clsx as _clsx } from 'clsx'
+import * as Clsx from '@nick/clsx'
 import { twMerge } from 'tailwind-merge'
 
-export function clsx<T extends ClassValue[]>(...inputs: T): string {
-	return twMerge(_clsx(inputs))
+declare module '@nick/clsx' {
+	export type ClassValues = readonly Clsx.ClassValue[]
+}
+
+export function clsx<const T extends Clsx.ClassValues>(...classes: [...T]): Clsx.Clsx<T>
+export function clsx(...classes: Clsx.ClassValues): string
+export function clsx(...classes: Clsx.ClassValues): string {
+	return twMerge(Clsx.clsx(classes))
 }
 
 // biome-ignore lint/performance/noBarrelFile: need a barrel file
