c49c33a2d069518b22a0497f5e6da0849aa37397
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-12-08 01:00:27 +0100

    feat(web): add GeneralErrorBoundary component
    Introduce a GeneralErrorBoundary component to handle route errors in
    the web app. It provides customizable handling for specific status
    codes and unexpected errors, improving error management and user
    experience.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

---
+++ apps/web/app/shell/general-error-boundary.tsx
@@ -0,0 +1,61 @@
+import type { ReactElement } from 'react'
+import { type ErrorResponse, isRouteErrorResponse, useParams, useRouteError } from 'react-router'
+
+function getErrorMessage(error: unknown): string {
+	if (typeof error === 'string') {
+		return error
+	}
+	if (
+		error &&
+		typeof error === 'object' &&
+		'message' in error &&
+		typeof error.message === 'string'
+	) {
+		return error.message
+	}
+	// biome-ignore lint/suspicious/noConsole: <explanation>
+	console.error('Unable to get error message for error', error)
+	return 'Unknown Error'
+}
+
+type StatusHandler = (info: {
+	error: ErrorResponse
+	params: Record<string, string | undefined>
+}) => ReactElement | null
+
+interface GeneralErrorBoundaryProps {
+	defaultStatusHandler?: StatusHandler
+	statusHandlers?: Record<number, StatusHandler>
+	unexpectedErrorHandler?: (error: unknown) => ReactElement | null
+}
+
+const DefaultStatusHandler: StatusHandler = ({ error }) => (
+	<p>
+		{error.status} {error.data}
+	</p>
+)
+
+export function GeneralErrorBoundary({
+	defaultStatusHandler = DefaultStatusHandler,
+	statusHandlers,
+	unexpectedErrorHandler = error => <p>{getErrorMessage(error)}</p>,
+}: GeneralErrorBoundaryProps): ReactElement {
+	const error = useRouteError()
+	const params = useParams()
+
+	if (typeof document !== 'undefined') {
+		// biome-ignore lint/suspicious/noConsole: <explanation>
+		console.error(error)
+	}
+
+	return (
+		<div className="container flex items-center justify-center p-20 text-h2">
+			{isRouteErrorResponse(error)
+				? (statusHandlers?.[error.status] ?? defaultStatusHandler)({
+						error,
+						params,
+					})
+				: unexpectedErrorHandler(error)}
+		</div>
+	)
+}
