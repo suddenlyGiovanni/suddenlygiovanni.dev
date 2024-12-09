8148f1b3b5cb7c021a274ef12a59c1cca98ff4ef
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-12-09 10:25:42 +0100

    feat(ui): wrap layout body content with a div
    Add a div to wrap the content inside Layout.Body to ensure consistent
     styling and structure. This change improves the encapsulation of
     nested components like Header, Main, and Footer within the Body
     component.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

--- packages/ui/src/components/layout/body.stories.tsx
+++ packages/ui/src/components/layout/body.stories.tsx
@@ -16,19 +16,24 @@ export const Body = {
 			<Layout.Body
 				{...args}
 				className="border-violet-500 border-dashed bg-violet-200 text-center text-violet-500 text-xl"
+				asChild={true}
 			>
-				Body
-				<Layout.Header>
-					<Placeholder className="border-green-500 bg-green-200 text-green-500">Header</Placeholder>
-				</Layout.Header>
-				<Layout.Main>
-					<Placeholder className="border-sky-500 bg-sky-200 text-sky-500">Main</Placeholder>
-				</Layout.Main>
-				<Layout.Footer>
-					<Placeholder className="border-purple-500 bg-purple-200 text-purple-500">
-						Footer
-					</Placeholder>
-				</Layout.Footer>
+				<div>
+					Body
+					<Layout.Header>
+						<Placeholder className="border-green-500 bg-green-200 text-green-500">
+							Header
+						</Placeholder>
+					</Layout.Header>
+					<Layout.Main>
+						<Placeholder className="border-sky-500 bg-sky-200 text-sky-500">Main</Placeholder>
+					</Layout.Main>
+					<Layout.Footer>
+						<Placeholder className="border-purple-500 bg-purple-200 text-purple-500">
+							Footer
+						</Placeholder>
+					</Layout.Footer>
+				</div>
 			</Layout.Body>
 		)
 	},
