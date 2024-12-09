092c53c42c98362ffd118827f6af7fa06306bcb9
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-12-09 02:40:31 +0100

    refactor(ui): replace forwardRef with FC for Avatar components
    Refactor Avatar components to use Function Component (FC) type
    instead of forwardRef. This simplifies the code and ensures better
    type inference. No change in behavior or functionality is expected.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

--- packages/ui/src/ui/avatar.tsx
+++ packages/ui/src/ui/avatar.tsx
@@ -1,34 +1,31 @@
 import { Fallback, Image, Root } from '@radix-ui/react-avatar'
-import { type ComponentPropsWithoutRef, type ElementRef, forwardRef } from 'react'
+import type { ComponentPropsWithRef, FC } from 'react'
 
 import { clsx } from '../lib/utils.ts'
 
-const Avatar = forwardRef<ElementRef<typeof Root>, ComponentPropsWithoutRef<typeof Root>>(
-	({ className, ...props }, ref) => (
-		<Root
-			className={clsx('relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full', className)}
-			ref={ref}
-			{...props}
-		/>
-	),
+const Avatar: FC<ComponentPropsWithRef<typeof Root>> = ({ className, ref, ...props }) => (
+	<Root
+		className={clsx('relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full', className)}
+		ref={ref}
+		{...props}
+	/>
 )
 Avatar.displayName = Root.displayName
 
-const AvatarImage = forwardRef<ElementRef<typeof Image>, ComponentPropsWithoutRef<typeof Image>>(
-	({ className, ...props }, ref) => (
-		<Image
-			className={clsx('aspect-square h-full w-full', className)}
-			ref={ref}
-			{...props}
-		/>
-	),
+const AvatarImage: FC<ComponentPropsWithRef<typeof Image>> = ({ className, ref, ...props }) => (
+	<Image
+		className={clsx('aspect-square h-full w-full', className)}
+		ref={ref}
+		{...props}
+	/>
 )
 AvatarImage.displayName = Image.displayName
 
-const AvatarFallback = forwardRef<
-	ElementRef<typeof Fallback>,
-	ComponentPropsWithoutRef<typeof Fallback>
->(({ className, ...props }, ref) => (
+const AvatarFallback: FC<ComponentPropsWithRef<typeof Fallback>> = ({
+	className,
+	ref,
+	...props
+}) => (
 	<Fallback
 		className={clsx(
 			'flex h-full w-full items-center justify-center rounded-full bg-muted',
@@ -37,7 +34,7 @@ const AvatarFallback = forwardRef<
 		ref={ref}
 		{...props}
 	/>
-))
+)
 AvatarFallback.displayName = Fallback.displayName
 
 export { Avatar, AvatarImage, AvatarFallback }
