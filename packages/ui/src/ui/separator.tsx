8a97574dec00da885532f58cafa1385d2f6a66aa
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-12-09 03:27:16 +0100

    feat(ui): refactor Separator component to use custom types
    Refactor the Separator component to utilize FC and
    CustomComponentPropsWithRef types. This change aims to enhance type
    safety and improve the component's clarity. Removed forwardRef usage
    for cleaner syntax and more straightforward prop management.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

--- packages/ui/src/ui/separator.tsx
+++ packages/ui/src/ui/separator.tsx
@@ -1,21 +1,25 @@
 import { Root } from '@radix-ui/react-separator'
-import { type ComponentPropsWithoutRef, type ElementRef, forwardRef } from 'react'
+import type { CustomComponentPropsWithRef, FC } from 'react'
 
 import { clsx } from '../lib/utils.ts'
 
-export const Separator = forwardRef<ElementRef<typeof Root>, ComponentPropsWithoutRef<typeof Root>>(
-	({ className, orientation = 'horizontal', decorative = true, ...props }, ref) => (
-		<Root
-			className={clsx(
-				'shrink-0 bg-border',
-				orientation === 'horizontal' ? 'h-[1px] w-full' : 'h-full w-[1px]',
-				className,
-			)}
-			decorative={decorative}
-			orientation={orientation}
-			ref={ref}
-			{...props}
-		/>
-	),
+export const Separator: FC<CustomComponentPropsWithRef<typeof Root>> = ({
+	className,
+	orientation = 'horizontal',
+	decorative = true,
+	ref,
+	...props
+}) => (
+	<Root
+		className={clsx(
+			'shrink-0 bg-border',
+			orientation === 'horizontal' ? 'h-[1px] w-full' : 'h-full w-[1px]',
+			className,
+		)}
+		decorative={decorative}
+		orientation={orientation}
+		ref={ref}
+		{...props}
+	/>
 )
 Separator.displayName = Root.displayName
