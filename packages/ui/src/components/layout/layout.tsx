de8bca2759317f02a59d911c1b688ae974464cdc
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-12-09 04:05:34 +0100

    refactor(ui): replace forwardRef with functional components
    Refactored layout components to use functional component syntax
    instead of forwardRef. This enhances readability and maintains
    functionality by utilizing React's ComponentPropsWithRef type for
    typing component props.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

--- packages/ui/src/components/layout/layout.tsx
+++ packages/ui/src/components/layout/layout.tsx
@@ -1,61 +1,61 @@
 import { Slot } from '@radix-ui/react-slot'
-import { type ComponentProps, type ElementRef, forwardRef } from 'react'
+import type { ComponentPropsWithRef, FC } from 'react'
+
 import { clsx } from '../../lib/utils.ts'
 
 const bodyName = 'Body'
-const Body = forwardRef<
-	ElementRef<'body'>,
-	ComponentProps<'body'> & {
-		asChild?: boolean
-	}
->(({ className, asChild = false, children, ...rest }, forwardedRef) => {
+const Body: FC<ComponentPropsWithRef<'body'> & { asChild?: boolean }> = ({
+	className,
+	asChild = false,
+	children,
+	ref,
+	...rest
+}) => {
 	const Comp = asChild ? Slot : 'body'
 	return (
 		<Comp
 			className={clsx('grid w-full auto-rows-auto grid-cols-1', className)}
 			data-testid={bodyName}
-			ref={forwardedRef}
+			ref={ref}
 			{...rest}
 		>
 			{children}
 		</Comp>
 	)
-})
+}
 Body.displayName = bodyName
 
 const headerName = 'Header'
-const Header = forwardRef<HTMLHeadElement, ComponentProps<'header'>>(
-	({ className, ...rest }, ref) => (
-		<header
-			className={clsx('row-start-1 row-end-2', className)}
-			data-testid={headerName}
-			ref={ref}
-			{...rest}
-		/>
-	),
+const Header: FC<ComponentPropsWithRef<'header'>> = ({ className, ref, ...rest }) => (
+	<header
+		className={clsx('row-start-1 row-end-2', className)}
+		data-testid={headerName}
+		ref={ref}
+		{...rest}
+	/>
 )
 Header.displayName = headerName
 
 const mainName = 'Main'
-const Main = forwardRef<HTMLElement, ComponentProps<'main'>>(({ className, ...rest }, ref) => (
+const Main: FC<ComponentPropsWithRef<'main'>> = ({ className, ref, ...rest }) => (
 	<main
 		className={clsx('row-start-2 row-end-3 min-h-screen overflow-y-auto', className)}
 		data-testid={mainName}
 		ref={ref}
 		{...rest}
 	/>
-))
+)
 Main.displayName = mainName
 
 const footerName = 'Footer'
-const Footer = forwardRef<HTMLElement, ComponentProps<'footer'>>(({ className, ...rest }, ref) => (
+const Footer: FC<ComponentPropsWithRef<'footer'>> = ({ className, ref, ...rest }) => (
 	<footer
 		className={clsx('row-start-3 row-end-4', className)}
 		data-testid={footerName}
 		{...rest}
 		ref={ref}
 	/>
-))
+)
 Footer.displayName = footerName
 
 export const Layout = {
