85e1eb3d559937d2dfc0b002fea113229e5e4095
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-12-11 16:44:36 +0100

    refactor(web): reorganize app directory to src
    Moved all files from the `app` directory to `src` to standardize the
    project structure. Updated relevant configurations (`react-router
    .config.ts`, `tsconfig.json`) to reflect the new directory layout.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

--- apps/web/react-router.config.ts
+++ apps/web/react-router.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from '@react-router/dev/config'
 
 export default {
-	appDirectory: 'app',
+	appDirectory: 'src',
 	serverModuleFormat: 'esm',
 	buildDirectory: 'build',
 	ssr: true,
