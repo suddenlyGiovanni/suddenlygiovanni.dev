388bc76239b27d8f5bcf0ea87fdab6b1867da7d4
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-12-14 23:58:04 +0100

    feat(ui): add eslint-plugin-storybook for linting support
    Integrates eslint-plugin-storybook to enhance linting for Storybook
    files. Updated `eslint.config.js` to include recommended
    configurations. This ensures better code consistency and adherence to
     best practices in Storybook usage.

--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,4 +1,6 @@
 import { config } from '@suddenlygiovanni/eslint-config/react-internal'
+import storybook from 'eslint-plugin-storybook'
 
 /** @type {import("eslint").Linter.Config} */
-export default config
+
+export default [...config, ...storybook.configs['flat/recommended']]
