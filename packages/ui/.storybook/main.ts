2aa2ebe71f34a514427d25bdcfe95786c1bc19bd
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-01-24 12:15:49 +0100

    refactor(ui): simplify Storybook config resolution
    Removed custom path resolution logic for Storybook addons and framework, relying directly on package names. This simplifies the configuration by eliminating unnecessary complexity and aligns with standard practices.

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,38 +1,22 @@
 import type { StorybookConfig } from '@storybook/react-vite'
 
-// biome-ignore lint/correctness/noNodejsModules: <explanation>
-import { dirname, join } from 'node:path'
-import { fileURLToPath } from 'node:url';
-
-
 async function defineConfig(config: StorybookConfig): Promise<StorybookConfig> {
 	return config
 }
 
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
-function getAbsolutePath(value: string) {
-	const modulePath = join(value, 'package.json');
-	const resolvedPath =  import.meta.resolve(modulePath);
-	return dirname(fileURLToPath(resolvedPath));
-}
-
-
 export default defineConfig({
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 	addons: [
-		 getAbsolutePath('@storybook/addon-onboarding'),
-		 getAbsolutePath('@storybook/addon-links'),
-		 getAbsolutePath('@storybook/addon-essentials'),
-		 getAbsolutePath('@chromatic-com/storybook'),
-		 getAbsolutePath('@storybook/addon-interactions'),
-		 getAbsolutePath('@storybook/addon-themes'),
-		 getAbsolutePath('@storybook/addon-a11y'),
+		 '@storybook/addon-onboarding',
+		 '@storybook/addon-links',
+		 '@storybook/addon-essentials',
+		 '@chromatic-com/storybook',
+		 '@storybook/addon-interactions',
+		 '@storybook/addon-themes',
+		 '@storybook/addon-a11y',
 	],
 	framework: {
-		name: getAbsolutePath('@storybook/react-vite') as '@storybook/react-vite',
+		name: '@storybook/react-vite',
 		options: {
 			strictMode: true,
 		},
