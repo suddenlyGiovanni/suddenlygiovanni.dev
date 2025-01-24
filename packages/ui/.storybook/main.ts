e3a4b92dcd49ad376528b6cd922da7b9066bf635
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-01-24 12:11:00 +0100

    refactor(ui): migrate Storybook config to async function
    Refactored the Storybook configuration to use an async function for better flexibility and modernization. Updated the `getAbsolutePath` function to leverage `import.meta.resolve` for resolving module paths. Simplified the default export with the new `defineConfig` wrapper.

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -2,24 +2,34 @@ import type { StorybookConfig } from '@storybook/react-vite'
 
 // biome-ignore lint/correctness/noNodejsModules: <explanation>
 import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url';
+
+
+async function defineConfig(config: StorybookConfig): Promise<StorybookConfig> {
+	return config
+}
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value: string): string {
-	return dirname(require.resolve(join(value, 'package.json')))
+function getAbsolutePath(value: string) {
+	const modulePath = join(value, 'package.json');
+	const resolvedPath =  import.meta.resolve(modulePath);
+	return dirname(fileURLToPath(resolvedPath));
 }
-const config: StorybookConfig = {
+
+
+export default defineConfig({
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 	addons: [
-		getAbsolutePath('@storybook/addon-onboarding'),
-		getAbsolutePath('@storybook/addon-links'),
-		getAbsolutePath('@storybook/addon-essentials'),
-		getAbsolutePath('@chromatic-com/storybook'),
-		getAbsolutePath('@storybook/addon-interactions'),
-		getAbsolutePath('@storybook/addon-themes'),
-		getAbsolutePath('@storybook/addon-a11y'),
+		 getAbsolutePath('@storybook/addon-onboarding'),
+		 getAbsolutePath('@storybook/addon-links'),
+		 getAbsolutePath('@storybook/addon-essentials'),
+		 getAbsolutePath('@chromatic-com/storybook'),
+		 getAbsolutePath('@storybook/addon-interactions'),
+		 getAbsolutePath('@storybook/addon-themes'),
+		 getAbsolutePath('@storybook/addon-a11y'),
 	],
 	framework: {
 		name: getAbsolutePath('@storybook/react-vite') as '@storybook/react-vite',
@@ -35,6 +45,4 @@ const config: StorybookConfig = {
 	typescript: {
 		reactDocgen: 'react-docgen-typescript',
 	},
-}
-
-export default config
+})
