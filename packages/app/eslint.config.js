bdefe2bb31ef82e99741cad6f8aa8b947492c57d
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-02-21 19:12:31 +0100

    chore(app): update eslint configuration and ignore files
    Add `.eslintignore` to exclude build artifacts and dependencies from
    linting. Enhance eslint setup by extending rules for test files,
    disabling `no-console` and `vitest/no-hooks` to allow more
    flexibility during tests.

--- packages/app/eslint.config.js
+++ packages/app/eslint.config.js
@@ -1,12 +1,16 @@
 import { config } from '@suddenly-giovanni/eslint-config/react-internal'
 import vitest from '@vitest/eslint-plugin'
 
-/** @type {import("eslint").Linter.Config} */
-
+/** @type {import("eslint").Linter.Config[]} */
 export default [
 	...config,
 	{
 		files: ['**/*.{spec,test}.{ts,tsx}'],
 		...vitest.configs.all,
+		rules: {
+			...vitest.configs.all.rules,
+			'no-console': 'off',
+			'vitest/no-hooks': 'off',
+		},
 	},
 ]
