542a233523c669912dc890284ccf9627dd6e8d1a
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-02-24 20:14:49 +0100

    Chore - Drop `turborepo` as workspace devDependancy (#1304)

--- tools/eslint-config/base.js
+++ tools/eslint-config/base.js
@@ -1,7 +1,6 @@
 import js from '@eslint/js'
 import eslintConfigPrettier from 'eslint-config-prettier'
 import onlyWarn from 'eslint-plugin-only-warn'
-import turboPlugin from 'eslint-plugin-turbo'
 import tseslint from 'typescript-eslint'
 
 /**
@@ -13,19 +12,7 @@ export const config = [
 	js.configs.recommended,
 	eslintConfigPrettier,
 	...tseslint.configs.recommended,
-	{
-		plugins: { turbo: turboPlugin },
-		rules: { 'turbo/no-undeclared-env-vars': 'warn' },
-	},
-	{
-		plugins: { onlyWarn },
-	},
-	{
-		ignores: ['dist/**'],
-	},
-	{
-		rules: {
-			'@typescript-eslint/no-namespace': 'off',
-		},
-	},
+	{ plugins: { onlyWarn } },
+	{ ignores: ['dist/**'] },
+	{ rules: { '@typescript-eslint/no-namespace': 'off' } },
 ]
