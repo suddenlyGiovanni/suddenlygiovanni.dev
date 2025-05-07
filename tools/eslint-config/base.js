f4aeb0e9d1d386e29cbe171206706c4ea9c6ac5f
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-05-07 15:44:29 +0200

    refactor(eslint-config): remove TypeScript and JS-specific rules
    Simplified ESLint configuration by excluding TypeScript and
    JS-specific rules and dependencies. Removed related packages,
    plugins, and redundant settings to streamline the setup and reduce
    complexity.

--- tools/eslint-config/base.js
+++ tools/eslint-config/base.js
@@ -1,18 +1,9 @@
-import js from '@eslint/js'
 import eslintConfigPrettier from 'eslint-config-prettier'
 import onlyWarn from 'eslint-plugin-only-warn'
-import tseslint from 'typescript-eslint'
 
 /**
  * A shared ESLint configuration for the repository.
  *
  * @type {import("eslint").Linter.Config[]}
  */
-export const config = [
-	js.configs.recommended,
-	eslintConfigPrettier,
-	...tseslint.configs.recommended,
-	{ plugins: { onlyWarn } },
-	{ ignores: ['dist/**'] },
-	{ rules: { '@typescript-eslint/no-namespace': 'off' } },
-]
+export const config = [eslintConfigPrettier, { plugins: { onlyWarn } }]
--- tools/eslint-config/react-internal.js
+++ tools/eslint-config/react-internal.js
@@ -1,8 +1,5 @@
-import js from '@eslint/js'
-import eslintConfigPrettier from 'eslint-config-prettier'
 import pluginReact from 'eslint-plugin-react'
 import * as reactHooks from 'eslint-plugin-react-hooks'
-import globals from 'globals'
 
 import { config as baseConfig } from './base.js'
 
@@ -12,26 +9,6 @@ import { config as baseConfig } from './base.js'
  * @type {import("eslint").Linter.Config[]} */
 export const config = [
 	...baseConfig,
-	js.configs.recommended,
-	eslintConfigPrettier,
 	pluginReact.configs.flat.recommended,
 	reactHooks.configs.recommended,
-	{
-		languageOptions: {
-			...pluginReact.configs.flat.recommended.languageOptions,
-			globals: {
-				...globals.serviceworker,
-				...globals.browser,
-			},
-		},
-	},
-	{
-		rules: {
-			// React scope no longer necessary with new JSX transform.
-			'react-hooks/react-compiler': 'error',
-			'react/no-unescaped-entities': 'off',
-			'react/react-in-jsx-scope': 'off',
-		},
-		settings: { react: { version: 'detect' } },
-	},
 ]
