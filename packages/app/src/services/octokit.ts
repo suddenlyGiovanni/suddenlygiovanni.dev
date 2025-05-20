d58719bf1684e84076d809976df914a6d37ad2dd
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-05-20 18:59:41 +0200

    feat(app): add `getContent` method to Octokit service
    Introduce a new `getContent` method for retrieving repository content
    . This method supports optional `ref` handling and integrates with
    the existing `use` function, enabling robust error management and
    request configuration.

--- packages/app/src/services/octokit.ts
+++ packages/app/src/services/octokit.ts
@@ -1,7 +1,9 @@
 import * as Api from '@octokit/core'
-import { restEndpointMethods } from '@octokit/plugin-rest-endpoint-methods'
-import { RequestError } from '@octokit/request-error'
-import { Config, Effect, Redacted, Schema } from 'effect'
+import {
+	type RestEndpointMethodTypes,
+	restEndpointMethods,
+} from '@octokit/plugin-rest-endpoint-methods'
+import { Config, Effect, Option, Redacted, Schema } from 'effect'
 
 export class OctokitError extends Schema.TaggedError<OctokitError>()('OctokitError', {
 	cause: Schema.Defect,
@@ -10,23 +12,39 @@ export class OctokitError extends Schema.TaggedError<OctokitError>()('OctokitErr
 export class Octokit extends Effect.Service<Octokit>()('app/services/Octokit', {
 	effect: Effect.gen(function* () {
 		const OctokitApi = Api.Octokit.plugin(restEndpointMethods)
-		const _client = new OctokitApi({ auth: Redacted.value(yield* Config.redacted('GITHUB_TOKEN')) })
+		const octokitApi = new OctokitApi({
+			auth: Redacted.value(yield* Config.redacted('GITHUB_TOKEN')),
+		})
 
 		const use = Effect.fn('Octokit.use')(
 			<A>(
-				f: (client: typeof _client, signal: AbortSignal) => Promise<A>,
+				f: (client: typeof octokitApi, signal: AbortSignal) => Promise<A>,
 			): Effect.Effect<A, OctokitError> =>
 				Effect.tryPromise({
-					catch: error => {
-						if (error instanceof RequestError) {
-							return new OctokitError({ cause: error })
-						}
-						return new OctokitError({ cause: error })
-					},
-					try: signal => f(_client, signal),
+					catch: cause => new OctokitError({ cause }),
+					try: signal => f(octokitApi, signal),
 				}),
 		)
 
-		return { client: _client, use } as const
+		const getContent: (
+			this: Octokit,
+			params: { ref: Option.Option<string>; repo: string; path: string; owner: string },
+		) => Effect.Effect<RestEndpointMethodTypes['repos']['getContent']['response'], OctokitError> =
+			Effect.fn('Octokit.getContent')(({ owner, path, repo, ref }) =>
+				use(({ rest }, abortSignal) =>
+					rest.repos.getContent({
+						owner,
+						path,
+						repo,
+						request: { signal: abortSignal },
+						...Option.match(ref, {
+							onNone: () => ({}),
+							onSome: _ref => ({ ref: _ref }),
+						}),
+					}),
+				),
+			)
+
+		return { client: octokitApi, getContent, use } as const
 	}),
 }) {}
