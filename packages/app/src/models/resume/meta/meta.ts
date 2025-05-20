9903b726bc6d7a234f5648ce9421f1602abf16ad
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-05-20 19:02:16 +0200

    refactor(app): simplify ResumeRepository with GithubService
    Replaces custom `getResumeFile` logic in `ResumeRepository` with the
    more modular `GithubService`. Adjusts schema definitions for `Meta`
    to improve flexibility, including optional fields with nullable support.

--- packages/app/src/models/resume/meta/meta.ts
+++ packages/app/src/models/resume/meta/meta.ts
@@ -2,18 +2,16 @@ import { TrimmedNonEmpty, UrlString } from '@suddenly-giovanni/schema-resume'
 import { Schema } from 'effect'
 
 export class Meta extends Schema.Class<Meta>('Meta')({
-	canonical: Schema.optionalWith(UrlString, { exact: true }),
-
-	lastModified: Schema.optionalWith(Schema.Date, { exact: true }).annotations({
-		jsonSchema: {
-			format: 'date-time',
-			type: 'string',
-		},
+	canonical: Schema.optionalWith(UrlString, {
+		nullable: true,
 	}),
 
-	version: Schema.optionalWith(TrimmedNonEmpty, { exact: true }).annotations({
+	lastModified: Schema.optional(Schema.Date).annotations({
+		jsonSchema: { format: 'date-time', type: 'string' },
+	}),
+
+	version: TrimmedNonEmpty.annotations({
 		description: 'A version field which follows semver - e.g. v1.0.0',
-		examples: ['v1.0.0'],
 		title: 'version',
 	}),
 }) {
