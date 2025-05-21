4074697cbb29fd869c158283077b49c87755ff7c
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-05-21 16:01:15 +0200

    refactor(utils): simplify loaderFunction implementation
    Refactored the loaderFunction for improved clarity and type adherence. Replaced generic parameter handling with a more specific interface for LoaderFunctionArgs, reducing complexity and potential type errors.

--- packages/app/src/lib/utils.ts
+++ packages/app/src/lib/utils.ts
@@ -1,15 +1,13 @@
 import { type Effect, type Layer, ManagedRuntime } from 'effect'
-import type { LoaderFunction } from 'react-router'
+import type { AppLoadContext, LoaderFunctionArgs } from 'react-router'
 
 export const makeRemixRuntime = <R, E>(layer: Layer.Layer<R, E, never>) => {
 	const runtime = ManagedRuntime.make(layer)
 
-	const loaderFunction: <A, E>(
-		body: (...args: Parameters<LoaderFunction>) => Effect.Effect<A, E, R>,
-	) => (...args: Parameters<LoaderFunction>) => Promise<A> =
-		body =>
-		(...args) =>
-			runtime.runPromise(body(...args))
+	const loaderFunction =
+		<A, EffectError>(loaderFunction: (arg: LoaderFunctionArgs<AppLoadContext>) => Effect.Effect<A, EffectError, R>) =>
+		(arg: LoaderFunctionArgs<AppLoadContext>): Promise<A> =>
+			runtime.runPromise(loaderFunction(arg))
 
 	return { loaderFunction }
 }
