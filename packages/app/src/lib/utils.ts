1b7ec747fd0d039e70430d0d41169bfb1ba27641
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-05-22 01:04:40 +0200

    feat(utils): add server action and loader helpers
    Introduced `makeServerActionFunction` and updated
    `makServerLoaderFunction` in `makeRemixRuntime` for better handling
    of server actions and loaders. Adjusted usage across services and
    routes to align with the new API.

--- packages/app/src/lib/utils.ts
+++ packages/app/src/lib/utils.ts
@@ -1,15 +1,22 @@
 import { type Effect, type Layer, ManagedRuntime } from 'effect'
-import type { LoaderFunction } from 'react-router'
+import type * as T from 'react-router'
 
-export const makeRemixRuntime = <R, E>(layer: Layer.Layer<R, E, never>) => {
+export const makeRemixRuntime = <R, LayerError>(layer: Layer.Layer<R, LayerError, never>) => {
 	const runtime = ManagedRuntime.make(layer)
 
-	const loaderFunction: <A, E>(
-		body: (...args: Parameters<LoaderFunction>) => Effect.Effect<A, E, R>,
-	) => (...args: Parameters<LoaderFunction>) => Promise<A> =
-		body =>
-		(...args) =>
-			runtime.runPromise(body(...args))
+	const makServerLoaderFunction =
+		<A, E, Arg extends T.LoaderFunctionArgs = T.LoaderFunctionArgs<T.AppLoadContext>>(
+			loader: (arg: Arg) => Effect.Effect<A, E, R>,
+		) =>
+		(arg: Arg): Promise<A> =>
+			runtime.runPromise(loader(arg))
 
-	return { loaderFunction }
+	const makeServerActionFunction =
+		<A, E, Arg extends T.ActionFunctionArgs = T.ActionFunctionArgs<T.AppLoadContext>>(
+			action: (arg: Arg) => Effect.Effect<A, E, R>,
+		) =>
+		(arg: Arg): Promise<A> =>
+			runtime.runPromise(action(arg))
+
+	return { makeServerActionFunction, makServerLoaderFunction }
 }
