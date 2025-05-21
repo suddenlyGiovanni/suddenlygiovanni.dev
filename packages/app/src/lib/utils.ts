aa21fbc61e6db40ac644ec2622664fcc9693a95b
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-05-21 17:04:01 +0200

    feat(utils): enhance type flexibility for loaderFunction
    Refactor `loaderFunction` to improve type definitions, enhancing clarity and compatibility with extended `react-router` types. This change introduces more generic type parameters to better handle various `LoaderFunctionArgs` use cases.

--- packages/app/src/lib/utils.ts
+++ packages/app/src/lib/utils.ts
@@ -1,12 +1,18 @@
 import { type Effect, type Layer, ManagedRuntime } from 'effect'
-import type { AppLoadContext, LoaderFunctionArgs } from 'react-router'
+import type * as T from 'react-router'
 
 export const makeRemixRuntime = <R, E>(layer: Layer.Layer<R, E, never>) => {
 	const runtime = ManagedRuntime.make(layer)
 
 	const loaderFunction =
-		<A, EffectError>(loaderFunction: (arg: LoaderFunctionArgs<AppLoadContext>) => Effect.Effect<A, EffectError, R>) =>
-		(arg: LoaderFunctionArgs<AppLoadContext>): Promise<A> =>
+		<
+			A,
+			EffectError,
+			LoaderFunctionArgs extends T.LoaderFunctionArgs = T.LoaderFunctionArgs<T.AppLoadContext>,
+		>(
+			loaderFunction: (arg: LoaderFunctionArgs) => Effect.Effect<A, EffectError, R>,
+		) =>
+		(arg: LoaderFunctionArgs): Promise<A> =>
 			runtime.runPromise(loaderFunction(arg))
 
 	return { loaderFunction }
