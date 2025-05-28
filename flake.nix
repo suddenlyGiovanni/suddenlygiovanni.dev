76d5b60375565ef1411ddf2383deaef7ddaccb0d
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-05-28 21:27:08 +0200

    chore(flakes): update nixpkgs lock and refactor flake outputs
    Updated nixpkgs lockfile to the latest revision and modernized flake
    .nix structure. Introduced `forAllSystems` for generating devShells
    across multiple systems, streamlining configuration and improving
    maintainability.

--- flake.nix
+++ flake.nix
@@ -1,29 +1,26 @@
 {
-  description = "`suddenlygiovanni.dev` darwin development environment";
-
   # Flake inputs
   inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
 
   # Flake outputs
-  outputs = { self, nixpkgs }:
+  outputs = { nixpkgs, ... }:
     let
-      system = "aarch64-darwin";
-      pkgs = import nixpkgs { inherit system; };
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+            system: function nixpkgs.legacyPackages.${system}
+        );
     in
     {
-      devShells.aarch64-darwin.default = pkgs.mkShell {
-        # The Nix packages provided in the environment
-        # Add any you need here
+
+      devShells = forAllSystems (pkgs: {
+       default = pkgs.mkShell {
         packages = with pkgs; [
           nodejs-slim_24
           corepack_24
           biome
         ];
-
-        # Set any environment variables for your dev shell
         env = { };
-
-        # Add any shell logic you want executed any time the environment is activated
         shellHook = ''
           echo "Nix dev env!"
           echo "Node version: $(node --version)"
@@ -31,6 +28,8 @@
           echo "Corepack version: $(corepack --version)"
           echo "pnpm version: $(pnpm -v)"
         '';
-      };
+       };
+
+      });
     };
 }
